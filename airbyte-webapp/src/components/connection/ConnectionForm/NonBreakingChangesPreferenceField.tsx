6b6a4c5e6b934b7edb21b2a32d047549b6cd0aa4
Volodymyr Petrov <dizel852@gmail.com>
2023-02-20 21:11:03 +0200

    replace align style classes with FormFieldWrapper and add missing disabled state to NonBreakingChangesPreferenceField component

--- airbyte-webapp/src/components/connection/ConnectionForm/NonBreakingChangesPreferenceField.tsx
+++ airbyte-webapp/src/components/connection/ConnectionForm/NonBreakingChangesPreferenceField.tsx
@@ -1,16 +1,14 @@
-import classNames from "classnames";
 import { FieldProps } from "formik";
 import { useMemo } from "react";
 import { useIntl } from "react-intl";
 
 import { ControlLabels } from "components";
 import { DropDown } from "components/ui/DropDown";
-import { FlexContainer } from "components/ui/Flex";
 
 import { NonBreakingChangesPreference } from "core/request/AirbyteClient";
 import { useConnectionFormService } from "hooks/services/ConnectionForm/ConnectionFormService";
 
-import styles from "./NonBreakingChangesPreferenceField.module.scss";
+import { FormFieldWrapper } from "./FormFieldWrapper";
 
 const SUPPORTED_PREFERENCES = [NonBreakingChangesPreference.ignore, NonBreakingChangesPreference.disable];
 
@@ -28,28 +26,25 @@ export const NonBreakingChangesPreferenceField: React.FC<FieldProps<string>> = (
   const { mode } = useConnectionFormService();
 
   return (
-    <FlexContainer alignItems="center">
-      <div className={styles.leftFieldCol}>
-        <ControlLabels
-          nextLine
-          label={formatMessage({
-            id: "connectionForm.nonBreakingChangesPreference.label",
-          })}
-          infoTooltipContent={formatMessage({
-            id: "connectionForm.nonBreakingChangesPreference.message",
-          })}
-        />
-      </div>
-      <div className={classNames(styles.rightFieldCol, { [styles.disabled]: mode === "readonly" })}>
-        <DropDown
-          {...field}
-          options={preferenceOptions}
-          error={form.touched[field.name] && !!form.errors[field.name]}
-          data-testid="nonBreakingChangesPreference"
-          value={field.value}
-          onChange={({ value }) => form.setFieldValue(field.name, value)}
-        />
-      </div>
-    </FlexContainer>
+    <FormFieldWrapper>
+      <ControlLabels
+        nextLine
+        label={formatMessage({
+          id: "connectionForm.nonBreakingChangesPreference.label",
+        })}
+        infoTooltipContent={formatMessage({
+          id: "connectionForm.nonBreakingChangesPreference.message",
+        })}
+      />
+      <DropDown
+        {...field}
+        options={preferenceOptions}
+        error={form.touched[field.name] && !!form.errors[field.name]}
+        data-testid="nonBreakingChangesPreference"
+        value={field.value}
+        isDisabled={form.isSubmitting || mode === "readonly"}
+        onChange={({ value }) => form.setFieldValue(field.name, value)}
+      />
+    </FormFieldWrapper>
   );
 };
