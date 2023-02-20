9a947be2d2eb8f31968935d332263be18906f41c
Ben Church <ben@airbyte.io>
2023-02-20 09:46:14 -0800

    Fix constant

--- airbyte-commons-server/src/main/java/io/airbyte/commons/server/services/AirbyteRemoteOssCatalog.java
+++ airbyte-commons-server/src/main/java/io/airbyte/commons/server/services/AirbyteRemoteOssCatalog.java
@@ -4,6 +4,7 @@
 
 package io.airbyte.commons.server.services;
 
+import io.airbyte.commons.constants.AirbyteCatalogConstants;
 import io.airbyte.config.CatalogDefinitionsConfig;
 import io.airbyte.config.init.RemoteDefinitionsProvider;
 import io.airbyte.config.specs.CombinedConnectorCatalogDownloader;
@@ -18,14 +19,12 @@ import org.slf4j.LoggerFactory;
 @Deprecated(forRemoval = true)
 @SuppressWarnings("PMD.AvoidCatchingThrowable")
 public class AirbyteRemoteOssCatalog extends RemoteDefinitionsProvider {
-
-  private static final String REMOTE_OSS_CATALOG_URL = CatalogDefinitionsConfig.getRemoteOssCatalogUrl();
   private static final long TIMEOUT = 30000;
   private static final Logger LOGGER = LoggerFactory.getLogger(CombinedConnectorCatalogDownloader.class);
 
   public AirbyteRemoteOssCatalog() {
-    super(REMOTE_OSS_CATALOG_URL, TIMEOUT);
-    LOGGER.info("Initializing OSS catalog from {} with timeout {}", REMOTE_OSS_CATALOG_URL, TIMEOUT);
+    super(AirbyteCatalogConstants.REMOTE_OSS_CATALOG_URL, TIMEOUT);
+    LOGGER.info("Initializing OSS catalog from {} with timeout {}", AirbyteCatalogConstants.REMOTE_OSS_CATALOG_URL, TIMEOUT);
   }
 
 }
