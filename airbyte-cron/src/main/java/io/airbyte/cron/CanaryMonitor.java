95fc0c24dc1fe4cd9b864a79ea1da2119dd2c9a0
pmossman <parker@airbyte.io>
2023-02-21 17:11:22 -0800

    start to add cron

---
+++ airbyte-cron/src/main/java/io/airbyte/cron/CanaryMonitor.java
@@ -0,0 +1,9 @@
+package io.airbyte.cron;
+
+/**
+ * The CanaryMonitor cron periodically examines configured
+ * workspace IDs and/or connection IDs for failures.
+ */
+public class CanaryMonitor {
+
+}
