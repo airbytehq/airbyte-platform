eca9ce3ae74404f4c2eae5d46de0b46068114bd9
KC <krishna@airbyte.io>
2023-02-23 13:09:13 -0500

    Tooltip

--- airbyte-webapp/src/components/EntityTable/components/StreamStatusCell.tsx
+++ airbyte-webapp/src/components/EntityTable/components/StreamStatusCell.tsx
@@ -10,13 +10,13 @@ import { useGetConnection } from "hooks/services/useConnectionHook";
 import styles from "./StreamStatusCell.module.scss";
 import { ConnectionTableDataItem } from "../types";
 
-type StatusType = "active" | "disabled" | "error" | "behind";
+type StatusType = "On Track" | "Disabled" | "Error" | "Behind";
 
 const statusMap: Readonly<Record<StatusType, string>> = {
-  active: styles.onTrack,
-  disabled: styles.disabled,
-  error: styles.error,
-  behind: styles.behind,
+  "On Track": styles.onTrack,
+  Disabled: styles.disabled,
+  Error: styles.error,
+  Behind: styles.behind,
 };
 
 const getStatusType = (connection: WebBackendConnectionRead): StatusType => {
@@ -32,30 +32,51 @@ const getStatusType = (connection: WebBackendConnectionRead): StatusType => {
           .subtract(connection.scheduleData.basicSchedule.units, connection.scheduleData.basicSchedule.timeUnit)
           .valueOf()
     ) {
-      return "behind";
+      return "Behind";
     }
-    return "active";
+    return "On Track";
   } else if (connection.latestSyncJobStatus === "failed") {
-    return "error";
+    return "Error";
   }
-  return "disabled";
+  return "Disabled";
+};
+
+const StreamStatusCellTooltipContent = ({
+  connection,
+  statusType,
+}: {
+  connection: WebBackendConnectionRead;
+  statusType: StatusType;
+}) => {
+  const filling = classNames(styles.filling, statusMap[statusType]);
+  return (
+    <div className={styles.tooltipContainer}>
+      <div className={styles.bar}>
+        <div className={filling} />
+      </div>
+      <div className={styles.tooltipText}>
+        <div>
+          {connection.syncCatalog.streams.length} {statusType}
+        </div>
+        <div>{connection.isSyncing ? "Syncing" : null}</div>
+      </div>
+    </div>
+  );
 };
 
 export const StreamsStatusCell: React.FC<CellContext<ConnectionTableDataItem, unknown>> = ({ row }) => {
   const connection = useGetConnection(row.original.connectionId);
-  const filling = classNames(styles.filling, statusMap[getStatusType(connection)]);
+  const statusType = getStatusType(connection);
+  const filling = classNames(styles.filling, statusMap[statusType]);
   return (
     <Tooltip
       control={
-        <div className={styles.container}>
+        <div className={styles.bar}>
           <div className={filling} />
         </div>
       }
     >
-      <div className={styles.container} style={{ width: "100px" }}>
-        <div className={filling} />
-      </div>
+      <StreamStatusCellTooltipContent connection={connection} statusType={statusType} />
     </Tooltip>
   );
-  // return <>{connection.syncCatalog.streams.length}</>;
 };
