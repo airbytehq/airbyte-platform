b1f736c4da2170860036206aed4ca42f2020a8f8
lmossman <lake@airbyte.io>
2023-02-22 18:19:57 -0800

    add sorting by name functionality

--- airbyte-webapp/src/pages/connectorBuilder/ConnectorBuilderListPage/ConnectorBuilderListPage.tsx
+++ airbyte-webapp/src/pages/connectorBuilder/ConnectorBuilderListPage/ConnectorBuilderListPage.tsx
@@ -2,18 +2,22 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { createColumnHelper } from "@tanstack/react-table";
 import classNames from "classnames";
-import { useMemo } from "react";
+import queryString from "query-string";
+import { useCallback, useMemo } from "react";
 import { FormattedMessage } from "react-intl";
 import { useNavigate } from "react-router-dom";
 
 import { DefaultLogoCatalog } from "components/common/DefaultLogoCatalog";
 import { HeadTitle } from "components/common/HeadTitle";
+import { SortOrderEnum } from "components/EntityTable/types";
 import { Button } from "components/ui/Button";
 import { FlexContainer } from "components/ui/Flex";
 import { Heading } from "components/ui/Heading";
 import { NextTable } from "components/ui/NextTable";
+import { SortableTableHeader } from "components/ui/Table";
 import { Text } from "components/ui/Text";
 
+import { useQuery } from "hooks/useQuery";
 import { useListProjects } from "services/connectorBuilder/ConnectorBuilderProjectsService";
 import { useCurrentWorkspaceId } from "services/workspaces/WorkspacesService";
 
@@ -27,17 +31,61 @@ interface Project {
 
 export const ConnectorBuilderListPage: React.FC = () => {
   const navigate = useNavigate();
+  const query = useQuery<{ sortBy?: string; order?: SortOrderEnum }>();
   const workspaceId = useCurrentWorkspaceId();
   const projectsReadList = useListProjects(workspaceId);
   // TODO: set version based on activeDeclarativeManifestVersion once it is added to the API
   const projects = projectsReadList.projects.map((projectDetails) => ({ name: projectDetails.name, version: "draft" }));
+
+  const sortBy = query.sortBy || "name";
+  const sortOrder = query.order || SortOrderEnum.ASC;
+
+  const onSortClick = useCallback(
+    (field: string) => {
+      const order =
+        sortBy !== field ? SortOrderEnum.ASC : sortOrder === SortOrderEnum.ASC ? SortOrderEnum.DESC : SortOrderEnum.ASC;
+      navigate({
+        search: queryString.stringify(
+          {
+            sortBy: field,
+            order,
+          },
+          { skipNull: true }
+        ),
+      });
+    },
+    [navigate, sortBy, sortOrder]
+  );
+
+  const sortData = useCallback(
+    (a, b) => {
+      const result = a[sortBy].toLowerCase().localeCompare(b[sortBy].toLowerCase());
+
+      if (sortOrder === SortOrderEnum.DESC) {
+        return -1 * result;
+      }
+
+      return result;
+    },
+    [sortBy, sortOrder]
+  );
+
+  const sortedProjects = useMemo(() => projects.sort(sortData), [sortData, projects]);
 
   const columnHelper = createColumnHelper<Project>();
 
   const columns = useMemo(
     () => [
       columnHelper.accessor("name", {
-        header: () => <FormattedMessage id="connectorBuilder.listPage.name" />,
+        header: () => (
+          <SortableTableHeader
+            onClick={() => onSortClick("name")}
+            isActive={sortBy === "name"}
+            isAscending={sortOrder === SortOrderEnum.ASC}
+          >
+            <FormattedMessage id="connectorBuilder.listPage.name" />
+          </SortableTableHeader>
+        ),
         cell: (props) => (
           <FlexContainer alignItems="center">
             {/* TODO: replace with custom logos once available */}
@@ -55,7 +103,7 @@ export const ConnectorBuilderListPage: React.FC = () => {
         ),
       }),
     ],
-    [columnHelper]
+    [columnHelper, onSortClick, sortBy, sortOrder]
   );
 
   return (
@@ -70,7 +118,7 @@ export const ConnectorBuilderListPage: React.FC = () => {
             <FormattedMessage id="connectorBuilder.listPage.newConnector" />
           </Button>
         </FlexContainer>
-        <NextTable columns={columns} data={projects} />
+        <NextTable columns={columns} data={sortedProjects} />
       </FlexContainer>
     </>
   );
