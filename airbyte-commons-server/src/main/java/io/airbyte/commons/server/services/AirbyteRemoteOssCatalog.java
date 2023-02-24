933fe36954703245045d4dc81358120b1e614dcb
Ben Church <ben@airbyte.io>
2023-02-23 16:10:45 -0800

    Catch not found errors in Remote Oss Store

--- airbyte-commons-server/src/main/java/io/airbyte/commons/server/services/AirbyteRemoteOssCatalog.java
+++ airbyte-commons-server/src/main/java/io/airbyte/commons/server/services/AirbyteRemoteOssCatalog.java
@@ -5,9 +5,14 @@
 package io.airbyte.commons.server.services;
 
 import io.airbyte.commons.constants.AirbyteCatalogConstants;
+import io.airbyte.config.StandardDestinationDefinition;
+import io.airbyte.config.StandardSourceDefinition;
 import io.airbyte.config.init.RemoteDefinitionsProvider;
 import io.airbyte.config.specs.CombinedConnectorCatalogDownloader;
 import jakarta.inject.Singleton;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,9 +32,33 @@ public class AirbyteRemoteOssCatalog extends RemoteDefinitionsProvider {
     this(AirbyteCatalogConstants.REMOTE_OSS_CATALOG_URL);
   }
 
-  public AirbyteRemoteOssCatalog(String remoteCatalogUrl) {
+  public AirbyteRemoteOssCatalog(final String remoteCatalogUrl) {
     super(remoteCatalogUrl, TIMEOUT);
     LOGGER.info("Initializing OSS catalog from {} with timeout {}", AirbyteCatalogConstants.REMOTE_OSS_CATALOG_URL, TIMEOUT);
+  }
+
+  @Override
+  public List<StandardDestinationDefinition> getDestinationDefinitions() {
+    try {
+      return super.getDestinationDefinitions();
+    } catch (final Throwable e) {
+      LOGGER.warn(
+          "Unable to retrieve latest Destination list from Remote Catalog. Using the list bundled with Airbyte. This warning is expected if this Airbyte cluster does not have internet access.",
+          e);
+      return Collections.emptyList();
+    }
+  }
+
+  @Override
+  public List<StandardSourceDefinition> getSourceDefinitions() {
+    try {
+      return super.getSourceDefinitions();
+    } catch (final Throwable e) {
+      LOGGER.warn(
+          "Unable to retrieve latest Source list from Remote Catalog. Using the list bundled with Airbyte. This warning is expected if this Airbyte cluster does not have internet access.",
+          e);
+      return Collections.emptyList();
+    }
   }
 
 }
