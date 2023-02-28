f4c179a00ef0f34bef16b9491b3414f7a5e0ead1
Joe Reuter <joe@airbyte.io>
2023-02-28 17:35:40 +0100

    fix buttons

--- airbyte-webapp/src/components/connectorBuilder/Builder/BuilderList.tsx
+++ airbyte-webapp/src/components/connectorBuilder/Builder/BuilderList.tsx
@@ -41,6 +41,7 @@ export const BuilderList: React.FC<BuilderListProps> = ({ children, emptyItem, b
       ))}
       <div>
         <Button
+          type="button"
           variant="secondary"
           icon={<FontAwesomeIcon icon={faPlus} />}
           onClick={() => {
