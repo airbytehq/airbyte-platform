077ff763dce468d11d0e94e7c1e746460d627e21
Edmundo Ruiz Ghanem <168664+edmundito@users.noreply.github.com>
2023-02-28 11:22:03 -0500

    🪟 🚦 🔧 Upgrade airbyte-webapp-e2e to Cypress 12 (#156)
    * Upgrade e2e to cypress 12
    
    * Update connectorBuilder e2e test to be compatible with cypress 12

--- airbyte-webapp-e2e-tests/cypress/e2e/connectorBuilder.cy.ts
+++ airbyte-webapp-e2e-tests/cypress/e2e/connectorBuilder.cy.ts
@@ -10,8 +10,13 @@ import {
 } from "commands/connectorBuilder";
 import { initialSetupCompleted } from "commands/workspaces";
 
-describe("Connector builder", () => {
+describe("Connector builder", { testIsolation: false }, () => {
   before(() => {
+    // Updated for cypress 12 because connector builder uses local storage
+    // docs.cypress.io/guides/references/migration-guide#Simulating-Pre-Test-Isolation-Behavior
+    cy.clearLocalStorage();
+    cy.clearCookies();
+
     initialSetupCompleted();
     goToConnectorBuilderPage();
     startFromScratch();
