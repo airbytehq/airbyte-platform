d88174f5027f4b1cf61e6708b42168ef2fd8f9a3
tealjulia <larson.teal@gmail.com>
2023-02-28 10:22:18 -0500

    uncomment code from yesterday

--- airbyte-webapp/src/packages/cloud/views/credits/CreditsPage/components/CreditsUsage.tsx
+++ airbyte-webapp/src/packages/cloud/views/credits/CreditsPage/components/CreditsUsage.tsx
@@ -1,64 +1,72 @@
-import React from "react";
-import { FormattedMessage } from "react-intl";
+import React, { useMemo } from "react";
+import { FormattedMessage, useIntl } from "react-intl";
 
+import { BarChart } from "components/ui/BarChart";
 import { Card } from "components/ui/Card";
+import { FlexContainer } from "components/ui/Flex";
 
+import { useExperiment } from "hooks/services/Experiment";
+import { useCurrentWorkspace } from "hooks/services/useWorkspace";
+import { useGetCloudWorkspaceUsage } from "packages/cloud/services/workspaces/CloudWorkspacesService";
+
+import styles from "./CreditsUSage.module.scss";
+import UsagePerConnectionTable from "./UsagePerConnectionTable";
 import { UsagePerDayGraph } from "./UsagePerDayGraph";
 
-// const LegendLabels = ["value"];
+const LegendLabels = ["value"];
 // import {Mock}
 
 const CreditsUsage: React.FC = () => {
-  // const isBillingInsightsEnabled = useExperiment("billing.billingInsights", false);
+  const isBillingInsightsEnabled = useExperiment("billing.billingInsights", false);
 
-  // const { formatMessage, formatDate } = useIntl();
+  const { formatMessage, formatDate } = useIntl();
 
   // todo: the following (through the return statement) can be removed once billingInsights is enabled:
-  // const { workspaceId } = useCurrentWorkspace();
-  // const data = useGetCloudWorkspaceUsage(workspaceId);
+  const { workspaceId } = useCurrentWorkspace();
+  const data = useGetCloudWorkspaceUsage(workspaceId);
 
-  // const chartData = useMemo(
-  //   () =>
-  //     data?.creditConsumptionByDay?.map(({ creditsConsumed, date }) => ({
-  //       name: formatDate(new Date(date[0], date[1] - 1 /* zero-indexed */, date[2]), {
-  //         month: "short",
-  //         day: "numeric",
-  //       }),
-  //       value: creditsConsumed,
-  //     })),
-  //   [data, formatDate]
-  // );
+  const chartData = useMemo(
+    () =>
+      data?.creditConsumptionByDay?.map(({ creditsConsumed, date }) => ({
+        name: formatDate(new Date(date[0], date[1] - 1 /* zero-indexed */, date[2]), {
+          month: "short",
+          day: "numeric",
+        }),
+        value: creditsConsumed,
+      })),
+    [data, formatDate]
+  );
 
   return (
     <>
       <Card title={<FormattedMessage id="credits.totalUsage" />} lightPadding>
-        {/* {isBillingInsightsEnabled ? ( */}
-        <UsagePerDayGraph />
-        {/* // ) : (
-        //   <div className={styles.chartWrapper}>
-        //     {data?.creditConsumptionByDay?.length ? (
-        //       <BarChart
-        //         data={chartData}
-        //         legendLabels={LegendLabels}
-        //         xLabel={formatMessage({
-        //           id: "credits.date",
-        //         })}
-        //         yLabel={formatMessage({
-        //           id: "credits.amount",
-        //         })}
-        //       />
-        //     ) : (
-        //       <FlexContainer alignItems="center" justifyContent="center" className={styles.empty}>
-        //         <FormattedMessage id="credits.noData" />
-        //       </FlexContainer>
-        //     )}
-        //   </div>
-        // )} */}
+        {isBillingInsightsEnabled ? (
+          <UsagePerDayGraph />
+        ) : (
+          <div className={styles.chartWrapper}>
+            {data?.creditConsumptionByDay?.length ? (
+              <BarChart
+                data={chartData}
+                legendLabels={LegendLabels}
+                xLabel={formatMessage({
+                  id: "credits.date",
+                })}
+                yLabel={formatMessage({
+                  id: "credits.amount",
+                })}
+              />
+            ) : (
+              <FlexContainer alignItems="center" justifyContent="center" className={styles.empty}>
+                <FormattedMessage id="credits.noData" />
+              </FlexContainer>
+            )}
+          </div>
+        )}
       </Card>
 
-      {/* <Card title={<FormattedMessage id="credits.usagePerConnection" />} lightPadding className={styles.cardBlock}>
+      <Card title={<FormattedMessage id="credits.usagePerConnection" />} lightPadding className={styles.cardBlock}>
         <UsagePerConnectionTable />
-      </Card> */}
+      </Card>
     </>
   );
 };
