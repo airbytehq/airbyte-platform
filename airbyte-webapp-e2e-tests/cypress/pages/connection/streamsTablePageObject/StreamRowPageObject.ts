8bb441f23cd01f4cc0f47685136198f7938cc8b4
Volodymyr Petrov <dizel852@gmail.com>
2023-03-03 17:32:09 +0200

    convert stream pageObject utility functions to class methods

--- airbyte-webapp-e2e-tests/cypress/pages/connection/streamsTablePageObject/StreamRowPageObject.ts
+++ airbyte-webapp-e2e-tests/cypress/pages/connection/streamsTablePageObject/StreamRowPageObject.ts
@@ -2,56 +2,77 @@ const streamTableRow = (namespace: string, streamName: string) =>
   `div[data-testid='catalog-tree-table-row-${namespace}-${streamName}']`;
 const streamPanel = `div[data-testid='stream-details']`;
 const streamPanelCloseButton = `button[data-testid='stream-details-close-button']`;
-const streamSyncSwitch = `input[data-testid='sync-switch']`;
+const streamSyncSwitch = `input[data-testid='selected-switch']`;
 const sourceStreamNameCell = `div[data-testid='source-stream-name-cell']`;
 const destinationStreamNameCell = `div[data-testid='destination-stream-name-cell']`;
 const sourceNamespaceCell = `div[data-testid='source-namespace-cell']`;
 const destinationNamespaceCell = `div[data-testid='destination-namespace-cell']`;
 
 export class StreamRowPageObject {
-  static getStreamUtilityFunctions(namespace: string, streamName: string) {
-    const stream = streamTableRow(namespace, streamName);
+  stream: string;
+  namespace: string;
+  streamName: string;
 
-    const isStreamSyncEnabled = (expectedValue: boolean) =>
-      cy.get(stream).within(() => {
-        cy.get(streamSyncSwitch).should(`${expectedValue ? "" : "not."}be.checked`);
-      });
+  constructor(namespace: string, streamName: string) {
+    this.namespace = namespace;
+    this.streamName = streamName;
+    this.stream = streamTableRow(namespace, streamName);
 
-    const toggleStreamSync = () =>
-      cy.get(stream).within(() => {
-        cy.get(streamSyncSwitch).closest("label").click();
-      });
+    // bind for destructuring
+    this.isStreamSyncEnabled = this.isStreamSyncEnabled.bind(this);
+    this.toggleStreamSync = this.toggleStreamSync.bind(this);
+    this.isStreamRowHasRemovedStyle = this.isStreamRowHasRemovedStyle.bind(this);
+    this.checkSourceNamespace = this.checkSourceNamespace.bind(this);
+    this.checkSourceStreamName = this.checkSourceStreamName.bind(this);
+    this.checkDestinationNamespace = this.checkDestinationNamespace.bind(this);
+    this.checkDestinationStreamName = this.checkDestinationStreamName.bind(this);
+    this.openStreamPanel = this.openStreamPanel.bind(this);
+    this.closeStreamPanel = this.closeStreamPanel.bind(this);
+    this.isStreamPanelVisible = this.isStreamPanelVisible.bind(this);
+  }
 
-    const isStreamRowHasRemovedStyle = (expectedValue: boolean) =>
-      cy
-        .get(stream)
-        .invoke("attr", "class")
-        .should(`${expectedValue ? "" : "not."}match`, /removed/);
+  isStreamSyncEnabled(expectedValue: boolean) {
+    cy.get(this.stream).within(() => {
+      cy.get(streamSyncSwitch).should(`${expectedValue ? "" : "not."}be.checked`);
+    });
+  }
 
-    const checkSourceNamespace = () => cy.get(stream).within(() => cy.get(sourceNamespaceCell).contains(namespace));
-    const checkSourceStreamName = () => cy.get(stream).within(() => cy.get(sourceStreamNameCell).contains(streamName));
+  toggleStreamSync() {
+    cy.get(this.stream).within(() => {
+      cy.get(streamSyncSwitch).parent().click();
+    });
+  }
 
-    const checkDestinationNamespace = (expectedValue: string) =>
-      cy.get(stream).within(() => cy.get(destinationNamespaceCell).contains(expectedValue));
-    const checkDestinationStreamName = (expectedValue: string) =>
-      cy.get(stream).within(() => cy.get(destinationStreamNameCell).contains(expectedValue));
+  isStreamRowHasRemovedStyle(expectedValue: boolean) {
+    cy.get(this.stream)
+      .invoke("attr", "class")
+      .should(`${expectedValue ? "" : "not."}match`, /removed/);
+  }
 
-    const openStreamPanel = () => cy.get(stream).within(() => cy.get(destinationNamespaceCell).click());
-    const closeStreamPanel = () => cy.get(streamPanel).within(() => cy.get(streamPanelCloseButton).click());
-    const isStreamPanelVisible = (expectedValue: boolean) =>
-      cy.get(streamPanel).should(`${expectedValue ? "" : "not."}exist`);
+  checkSourceNamespace() {
+    cy.get(this.stream).within(() => cy.get(sourceNamespaceCell).contains(this.namespace));
+  }
 
-    return {
-      isStreamSyncEnabled,
-      toggleStreamSync,
-      isStreamRowHasRemovedStyle,
-      checkSourceNamespace,
-      checkSourceStreamName,
-      checkDestinationNamespace,
-      checkDestinationStreamName,
-      openStreamPanel,
-      closeStreamPanel,
-      isStreamPanelVisible,
-    };
+  checkSourceStreamName() {
+    cy.get(this.stream).within(() => cy.get(sourceStreamNameCell).contains(this.streamName));
+  }
+
+  checkDestinationNamespace(expectedValue: string) {
+    cy.get(this.stream).within(() => cy.get(destinationNamespaceCell).contains(expectedValue));
+  }
+  checkDestinationStreamName(expectedValue: string) {
+    cy.get(this.stream).within(() => cy.get(destinationStreamNameCell).contains(expectedValue));
+  }
+
+  openStreamPanel() {
+    cy.get(this.stream).within(() => cy.get(destinationNamespaceCell).click());
+  }
+
+  closeStreamPanel() {
+    cy.get(streamPanel).within(() => cy.get(streamPanelCloseButton).click());
+  }
+
+  isStreamPanelVisible(expectedValue: boolean) {
+    cy.get(streamPanel).should(`${expectedValue ? "" : "not."}exist`);
   }
 }
