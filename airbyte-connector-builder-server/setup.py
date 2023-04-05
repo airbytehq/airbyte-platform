659083268ff03c011fcaee3d12bc8da51eb248e2
gosusnp <gosusnp@users.noreply.github.com>
2023-04-05 20:28:07 +0000

    Bump Airbyte version from 0.42.1 to 0.43.0

--- airbyte-connector-builder-server/setup.py
+++ airbyte-connector-builder-server/setup.py
@@ -16,7 +16,7 @@ CDK_VERSION = (HERE.parent / "airbyte-connector-builder-resources" / "CDK_VERSIO
 
 setup(
     name="connector-builder-server",
-    version="0.42.1",
+    version="0.43.0",
     description="",
     long_description=README,
     author="Airbyte",
