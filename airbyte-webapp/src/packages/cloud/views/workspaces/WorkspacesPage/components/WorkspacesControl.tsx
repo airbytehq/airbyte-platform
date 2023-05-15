559e84c5c5c42151108ea2a51265f490b0da65b0
Teal Larson <LARSON.TEAL@GMAIL.COM>
2023-05-15 10:51:01 -0400

    🪟 🎉 Workspaces can be opened in a new tab (#6565)

--- airbyte-webapp/src/packages/cloud/views/workspaces/WorkspacesPage/components/WorkspacesControl.tsx
+++ airbyte-webapp/src/packages/cloud/views/workspaces/WorkspacesPage/components/WorkspacesControl.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
 import { useToggle } from "react-use";
-import styled from "styled-components";
 
+import { Box } from "components/ui/Box";
 import { Button } from "components/ui/Button";
 import { Card } from "components/ui/Card";
 
 import CreateWorkspaceForm from "./CreateWorkspaceForm";
 import styles from "./WorkspaceControl.module.scss";
 
-const FormContent = styled(Card)`
-  padding: 15px 20px 16px 20px;
-`;
-
-const WorkspacesControl: React.FC<{
+export const WorkspacesControl: React.FC<{
   onSubmit: (name: string) => Promise<unknown>;
 }> = (props) => {
   const [isEditMode, toggleMode] = useToggle(false);
@@ -23,15 +19,17 @@ const WorkspacesControl: React.FC<{
     toggleMode();
   };
 
-  return isEditMode ? (
-    <FormContent>
-      <CreateWorkspaceForm onSubmit={onSubmit} />
-    </FormContent>
-  ) : (
-    <Button className={styles.createButton} onClick={toggleMode} data-testid="workspaces.createNew">
-      <FormattedMessage id="workspaces.createNew" />
-    </Button>
+  return (
+    <Box pb="2xl">
+      {isEditMode ? (
+        <Card withPadding>
+          <CreateWorkspaceForm onSubmit={onSubmit} />
+        </Card>
+      ) : (
+        <Button className={styles.createButton} onClick={toggleMode} data-testid="workspaces.createNew">
+          <FormattedMessage id="workspaces.createNew" />
+        </Button>
+      )}
+    </Box>
   );
 };
-
-export default WorkspacesControl;
