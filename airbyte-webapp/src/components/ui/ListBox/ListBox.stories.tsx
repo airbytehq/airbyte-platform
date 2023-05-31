be53b4c591837e1f4a6199ea542befb722fd8bad
Vladimir <dizel852@gmail.com>
2023-05-31 18:41:05 +0300

    🪟 `<ListBox />` enhancements (#6259)
    Co-authored-by: lmossman <lake@airbyte.io>

--- airbyte-webapp/src/components/ui/ListBox/ListBox.stories.tsx
+++ airbyte-webapp/src/components/ui/ListBox/ListBox.stories.tsx
@@ -1,30 +1,73 @@
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryObj, StoryFn } from "@storybook/react";
+
+import { FlexContainer } from "components/ui/Flex";
 
 import { ListBox } from "./ListBox";
 
 export default {
   title: "Ui/ListBox",
   component: ListBox,
+  argTypes: {
+    placement: {
+      options: [
+        "top",
+        "top-start",
+        "top-end",
+        "right",
+        "right-start",
+        "right-end",
+        "bottom",
+        "bottom-start",
+        "bottom-end",
+        "left",
+        "left-start",
+        "left-end",
+      ],
+      control: { type: "radio" },
+    },
+  },
 } as Meta<typeof ListBox>;
 
-export const Primary: StoryObj<typeof ListBox> = {
-  args: {
-    options: [
-      {
-        label: "one",
-        value: 1,
-        icon: <FontAwesomeIcon icon={faEdit} />,
-      },
-      {
-        label: "two",
-        value: 2,
-      },
-      {
-        label: "three",
-        value: 3,
-      },
-    ],
+type Story = StoryObj<typeof ListBox>;
+
+const options = [
+  {
+    label: "one",
+    value: 1,
+    icon: <FontAwesomeIcon icon={faEdit} />,
   },
+  {
+    label: "two",
+    value: 2,
+  },
+  {
+    label: "three",
+    value: 3,
+  },
+];
+
+export const Primary: Story = {
+  args: {
+    options,
+  },
+};
+
+export const Placement: Story = {
+  args: {
+    options,
+    adaptiveWidth: false,
+  },
+  decorators: [
+    (Story: StoryFn) => (
+      <FlexContainer
+        alignItems="center"
+        justifyContent="center"
+        style={{ width: 500, height: 500, border: "1px solid #494961" }}
+      >
+        <Story />
+      </FlexContainer>
+    ),
+  ],
 };
