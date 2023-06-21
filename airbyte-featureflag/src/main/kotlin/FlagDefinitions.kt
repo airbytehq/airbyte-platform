2804df6cb701703fdd1f4942d8d6b5dca6bce9bd
Ella Rohm-Ensing <ella@airbyte.io>
2023-06-21 09:49:30 -0500

    Remove seed adv feature flag (#7361)

--- airbyte-featureflag/src/main/kotlin/FlagDefinitions.kt
+++ airbyte-featureflag/src/main/kotlin/FlagDefinitions.kt
@@ -32,7 +32,6 @@ object CheckWithCatalog : Temporary<Boolean>(key = "check-with-catalog", default
 object ConnectorVersionOverridesEnabled : Temporary<Boolean>(key = "connectors.versionOverridesEnabled", default = false)
 
 object UseActorDefinitionVersionTableDefaults : Temporary<Boolean>(key = "connectors.useActorDefinitionVersionTableDefaults", default = false)
-object SeedActorDefinitionVersions : Temporary<Boolean>(key = "connectors.seedActorDefinitionVersions", default = true)
 
 object MinimumCreditQuantity : Temporary<Int>(key = "minimum-credit-quantity", default = 100)
 
