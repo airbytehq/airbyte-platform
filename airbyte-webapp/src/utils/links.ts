3436d1c15d948f06648744941ccc4e22f81aec06
mauricioalarcon <>
2023-06-27 15:00:27 -0400

    Updating test from main upstream

--- airbyte-webapp/src/utils/links.ts
+++ airbyte-webapp/src/utils/links.ts
@@ -15,7 +15,7 @@ export const links = {
   updateLink: `${BASE_DOCS_LINK}/operator-guides/upgrading-airbyte`,
   productReleaseStages: `${BASE_DOCS_LINK}/project-overview/product-release-stages`,
   slackLink: "https://slack.airbyte.com",
-  supportTicketLink: "https://airbyte.com/contact-support",
+  supportPortal: "https://support.airbyte.com",
   docsLink: BASE_DOCS_LINK,
   configurationArchiveLink: `${BASE_DOCS_LINK}/operator-guides/upgrading-airbyte/`,
   normalizationLink: `${BASE_DOCS_LINK}/understanding-airbyte/connections#airbyte-basic-normalization`,
@@ -47,7 +47,11 @@ export const links = {
   connectorBuilderErrorHandler: `${BASE_DOCS_LINK}/connector-development/connector-builder-ui/error-handling`,
   connectorBuilderPartitioning: `${BASE_DOCS_LINK}/connector-development/connector-builder-ui/partitioning`,
   connectorBuilderTransformations: `${BASE_DOCS_LINK}/connector-development/connector-builder-ui/record-processing#transformations`,
+  connectorBuilderTutorial: `${BASE_DOCS_LINK}/connector-development/connector-builder-ui/tutorial`,
   interpolationVariableDocs: `${BASE_DOCS_LINK}/connector-development/config-based/understanding-the-yaml-file/reference`,
+  interpolationMacroDocs: `${BASE_DOCS_LINK}/connector-development/config-based/understanding-the-yaml-file/reference#macros`,
+  creditDescription: "https://airbyte.com/pricing#what-is-a-credit",
+  usingCustomConnectors: `${BASE_DOCS_LINK}/operator-guides/using-custom-connectors/`,
 } as const;
 
 export type OutboundLinks = typeof links;
