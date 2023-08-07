4ad3e97f337ffcfb2b9d2519ad489792afc9cf75
Ryan Br <ryan.broughan@gmail.com>
2023-08-07 10:38:33 -0700

    Rbroughan/log retry state part deux (#8141)

---
+++ airbyte-workers/src/main/java/io/airbyte/workers/temporal/scheduling/activities/AppendToAttemptLogActivityImpl.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.workers.temporal.scheduling.activities;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.airbyte.commons.temporal.TemporalUtils;
+import io.airbyte.config.Configs.WorkerEnvironment;
+import io.airbyte.config.helpers.LogClientSingleton;
+import io.airbyte.config.helpers.LogConfigs;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import java.nio.file.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Concrete implementation of AppendToAttemptLogActivity.
+ */
+@Singleton
+public class AppendToAttemptLogActivityImpl implements AppendToAttemptLogActivity {
+
+  @VisibleForTesting
+  protected Logger logger = LoggerFactory.getLogger(AppendToAttemptLogActivityImpl.class);
+  private final LogClientSingleton logClientSingleton;
+  private final LogConfigs logConfigs;
+  private final Path workspaceRoot;
+  private final WorkerEnvironment workerEnvironment;
+
+  public AppendToAttemptLogActivityImpl(
+                                        final LogClientSingleton logClientSingleton,
+                                        final LogConfigs logConfigs,
+                                        @Named("workspaceRoot") final Path workspaceRoot,
+                                        final WorkerEnvironment workerEnvironment) {
+    this.logClientSingleton = logClientSingleton;
+    this.logConfigs = logConfigs;
+    this.workspaceRoot = workspaceRoot;
+    this.workerEnvironment = workerEnvironment;
+  }
+
+  @Override
+  public LogOutput log(final LogInput input) {
+    if (input.getJobId() == null || input.getAttemptNumber() == null) {
+      return new LogOutput(false);
+    }
+
+    setMdc(input);
+
+    final var msg = input.getMessage();
+
+    switch (input.getLevel()) {
+      case ERROR -> logger.error(msg);
+      case WARN -> logger.warn(msg);
+      default -> logger.info(msg);
+    }
+
+    return new LogOutput(true);
+  }
+
+  private void setMdc(final LogInput input) {
+    final Path jobRoot = TemporalUtils.getJobRoot(
+        workspaceRoot,
+        String.valueOf(input.getJobId()),
+        input.getAttemptNumber());
+
+    logClientSingleton.setJobMdc(workerEnvironment, logConfigs, jobRoot);
+  }
+
+}
