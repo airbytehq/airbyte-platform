d060cda3ae88fd64e223a49aea1ec3e9db100fc3
Malik Diarra <malik@airbyte.io>
2023-08-09 18:28:59 -0700

    Add new connection status endpoint (#7793)

--- airbyte-server/src/main/java/io/airbyte/server/apis/ConnectionApiController.java
+++ airbyte-server/src/main/java/io/airbyte/server/apis/ConnectionApiController.java
@@ -15,6 +15,7 @@ import io.airbyte.api.model.generated.ConnectionIdRequestBody;
 import io.airbyte.api.model.generated.ConnectionRead;
 import io.airbyte.api.model.generated.ConnectionReadList;
 import io.airbyte.api.model.generated.ConnectionSearch;
+import io.airbyte.api.model.generated.ConnectionStatusRead;
 import io.airbyte.api.model.generated.ConnectionStreamRequestBody;
 import io.airbyte.api.model.generated.ConnectionUpdate;
 import io.airbyte.api.model.generated.InternalOperationResult;
@@ -35,6 +36,8 @@ import io.micronaut.http.annotation.Status;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
+import java.util.List;
+import java.util.UUID;
 
 @SuppressWarnings("MissingJavadocType")
 @Controller("/api/v1/connections")
@@ -131,6 +134,15 @@ public class ConnectionApiController implements ConnectionApi {
   @ExecuteOn(AirbyteTaskExecutors.IO)
   public ConnectionRead getConnection(@Body final ConnectionIdRequestBody connectionIdRequestBody) {
     return ApiHelper.execute(() -> connectionsHandler.getConnection(connectionIdRequestBody.getConnectionId()));
+  }
+
+  @Override
+  @Post(uri = "/connections/status")
+  @Secured({READER})
+  @SecuredWorkspace
+  @ExecuteOn(AirbyteTaskExecutors.IO)
+  public List<ConnectionStatusRead> getConnectionStatuses(@Body List<UUID> connectionIds) {
+    return ApiHelper.execute(() -> connectionsHandler.getConnectionStatuses(connectionIds));
   }
 
   @Override
