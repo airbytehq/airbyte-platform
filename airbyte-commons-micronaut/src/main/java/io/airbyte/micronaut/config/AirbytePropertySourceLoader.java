cb3a8f4f6c12502e18476f6d07482377ce68bc5a
Tim Roes <tim@airbyte.io>
2023-09-08 21:50:25 +0200

    Allow enterprise startup without IDP config (#8736)

--- airbyte-commons-micronaut/src/main/java/io/airbyte/micronaut/config/AirbytePropertySourceLoader.java
+++ airbyte-commons-micronaut/src/main/java/io/airbyte/micronaut/config/AirbytePropertySourceLoader.java
@@ -13,9 +13,9 @@ import io.micronaut.core.io.ResourceLoader;
 import io.micronaut.core.io.file.DefaultFileSystemResourceLoader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -50,8 +50,10 @@ public class AirbytePropertySourceLoader implements PropertySourceLoader {
       final var yaml = read(resourceName, stream);
 
       // Prefix all configuration from that file with airbyte.
-      final var prefixedProps = yaml.entrySet().stream().collect(Collectors.toMap(entry -> AIRBYTE_KEY + "." + entry.getKey(), Map.Entry::getValue));
-      log.info("Loading properties as: {}", prefixedProps);
+      final var prefixedProps = new HashMap<String, Object>();
+      for (var entry : yaml.entrySet()) {
+        prefixedProps.put(AIRBYTE_KEY + "." + entry.getKey(), entry.getValue());
+      }
       return Optional.of(PropertySource.of(prefixedProps));
     } catch (final IOException e) {
       throw new ConfigurationException("Could not load airbyte.yml configuration file.", e);
