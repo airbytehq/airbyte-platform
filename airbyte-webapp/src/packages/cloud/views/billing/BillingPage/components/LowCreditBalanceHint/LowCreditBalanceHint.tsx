b12a5f7b259d9d15ab5e6d0a01485b11b8dd7895
Teal Larson <LARSON.TEAL@GMAIL.COM>
2023-09-12 15:18:20 -0400

    🪟 🏁 (flagged) Adjust wording of low credits hints after FCP sunset (#8776)

--- airbyte-webapp/src/packages/cloud/views/billing/BillingPage/components/LowCreditBalanceHint/LowCreditBalanceHint.tsx
+++ airbyte-webapp/src/packages/cloud/views/billing/BillingPage/components/LowCreditBalanceHint/LowCreditBalanceHint.tsx
@@ -2,6 +2,8 @@ import { FormattedMessage } from "react-intl";
 
 import { Message } from "components/ui/Message";
 
+import { useIsFCPEnabled } from "core/api/cloud";
+
 interface LowCreditBalanceHintProps {
   variant: "info" | "warning" | "error";
 }
@@ -9,14 +11,13 @@ interface LowCreditBalanceHintProps {
 export const LOW_BALANCE_CREDIT_THRESHOLD = 20;
 
 export const LowCreditBalanceHint: React.FC<React.PropsWithChildren<LowCreditBalanceHintProps>> = ({ variant }) => {
+  const isFCPEnabled = useIsFCPEnabled();
   if (variant === "info") {
     return null;
   }
 
-  return (
-    <Message
-      text={<FormattedMessage id={variant === "error" ? "credits.zeroBalance" : "credits.lowBalance"} />}
-      type={variant}
-    />
-  );
+  const zeroBalanceId = isFCPEnabled ? "credits.zeroBalance.fcp" : "credits.zeroBalance";
+  const lowBalanceId = isFCPEnabled ? "credits.lowBalance.fcp" : "credits.lowBalance";
+
+  return <Message text={<FormattedMessage id={variant === "error" ? zeroBalanceId : lowBalanceId} />} type={variant} />;
 };
