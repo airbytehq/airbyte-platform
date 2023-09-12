99162c46a91b90f9b66a13080cf49999a9e1732d
keyihuang <keyi@airbyte.io>
2023-09-12 09:55:35 -0700

    Sunset FCP (Phase 1) (#8580)

--- airbyte-featureflag/src/main/kotlin/FlagDefinitions.kt
+++ airbyte-featureflag/src/main/kotlin/FlagDefinitions.kt
@@ -103,6 +103,8 @@ object HideActorDefinitionFromList : Permanent<Boolean>(key = "connectors.hideAc
 
 object PauseSyncsWithUnsupportedActors : Temporary<Boolean>(key = "connectors.pauseSyncsWithUnsupportedActors", default = true)
 
+object SunsetFCP : Temporary<Boolean>(key = "platform.sunset-fcp", default = false)
+
 object DestResourceOverrides : Temporary<String>(key = "dest-resource-overrides", default = "")
 
 object OrchestratorResourceOverrides : Temporary<String>(key = "orchestrator-resource-overrides", default = "")
