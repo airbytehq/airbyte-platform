a1776bbfddfeffc93fdceaa6d649225108b3a37b
Chandler Prall <chandler.prall@gmail.com>
2023-10-31 09:53:15 -0400

    🪟 🔧 Decouple intents for unit tests from runtime intents (#9535)

--- airbyte-webapp/src/core/utils/rbac/intent.ts
+++ airbyte-webapp/src/core/utils/rbac/intent.ts
@@ -1,24 +1,11 @@
+import { intentToRbacQuery } from "./intents";
 import { useRbac } from "./rbac";
 import { RbacQuery, RbacQueryWithoutResourceId, RbacResource } from "./rbacPermissionsQuery";
 
-const intentToRbacQuery = {
-  // instance
-
-  // organization
-  ListOrganizationMembers: { resourceType: "ORGANIZATION", role: "READER" },
-  UpdateOrganization: { resourceType: "ORGANIZATION", role: "ADMIN" },
-  UpdateOrganizationPermissions: { resourceType: "ORGANIZATION", role: "ADMIN" },
-  ViewOrganizationSettings: { resourceType: "ORGANIZATION", role: "READER" },
-
-  // workspace
-  DeleteWorkspace: { resourceType: "WORKSPACE", role: "ADMIN" },
-  ListWorkspaceMembers: { resourceType: "WORKSPACE", role: "READER" },
-  UpdateWorkspace: { resourceType: "WORKSPACE", role: "ADMIN" },
-  UpdateWorkspacePermissions: { resourceType: "WORKSPACE", role: "ADMIN" },
-  ViewWorkspaceSettings: { resourceType: "WORKSPACE", role: "READER" },
-} as const;
-
-export type Intent = keyof typeof intentToRbacQuery;
+type IntentToRbacQuery = typeof intentToRbacQuery;
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface AllIntents extends IntentToRbacQuery {}
+export type Intent = keyof AllIntents;
 
 interface OrganizationIntentMeta {
   organizationId?: string;
@@ -27,10 +14,10 @@ interface WorkspaceIntentMeta {
   workspaceId?: string;
 }
 
-// Utility types to enforce shape of any meta object; i.e. organzaition-focused intents shouldn't receive workspaceIds
+// Utility types to enforce shape of any meta object; i.e. organization-focused intents shouldn't receive workspaceIds
 // provides proper autocomplete hinting for `meta` and to future proof changes to Intent:Query mapping,
 // e.g. changing Intent X from resource:"WORKSPACE" to resource:"ORGANIZATION" should catch instances of invalid meta
-type MapIntentToResource<I extends Intent> = (typeof intentToRbacQuery)[I]["resourceType"];
+type MapIntentToResource<I extends Intent> = AllIntents[I]["resourceType"];
 type IntentMetaForResource<R extends RbacResource> = R extends "INSTANCE"
   ? Record<string, never>
   : R extends "ORGANIZATION"
@@ -44,7 +31,7 @@ Given the React context + overrides provided in optional `details`,
 determine if the user has the required permissions to perform the given `intent`.
 */
 export const useIntent = <I extends Intent>(intent: I, meta?: IntentMetaForResource<MapIntentToResource<I>>) => {
-  let query: RbacQuery | RbacQueryWithoutResourceId = intentToRbacQuery[intent];
+  let query: RbacQuery | RbacQueryWithoutResourceId = intentToRbacQuery[intent as keyof IntentToRbacQuery];
   if (isOrganizationIntentMeta(query.resourceType, meta) && meta?.organizationId) {
     query = { ...query, resourceId: meta?.organizationId };
   } else if (isWorkspaceIntentMeta(query.resourceType, meta) && meta?.workspaceId) {
