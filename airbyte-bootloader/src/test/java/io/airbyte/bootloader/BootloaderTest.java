dc901be079decd02ed71fd502cff77e7e7e2535a
Benoit Moriceau <benoit@airbyte.io>
2023-11-01 11:35:27 -0700

    Add workload table (#9460)

--- airbyte-bootloader/src/test/java/io/airbyte/bootloader/BootloaderTest.java
+++ airbyte-bootloader/src/test/java/io/airbyte/bootloader/BootloaderTest.java
@@ -90,7 +90,7 @@ class BootloaderTest {
 
   // ⚠️ This line should change with every new migration to show that you meant to make a new
   // migration to the prod database
-  private static final String CURRENT_CONFIGS_MIGRATION_VERSION = "0.50.24.009";
+  private static final String CURRENT_CONFIGS_MIGRATION_VERSION = "0.50.33.001";
   private static final String CURRENT_JOBS_MIGRATION_VERSION = "0.50.4.001";
   private static final String CDK_VERSION = "1.2.3";
 
