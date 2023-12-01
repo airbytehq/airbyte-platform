76144dd71b6b5f38c36b95807e6b87364dbfe776
Teal Larson <LARSON.TEAL@GMAIL.COM>
2023-12-01 15:22:55 -0500

    🪟 🧹  Clean up unused hooks from api/hooks (#10098)

--- airbyte-webapp/src/core/api/hooks/index.ts
+++ airbyte-webapp/src/core/api/hooks/index.ts
@@ -11,7 +11,6 @@ export * from "./geographies";
 export * from "./health";
 export * from "./instanceConfiguration";
 export * from "./jobs";
-export * from "./logs";
 export * from "./notifications";
 export * from "./operations";
 export * from "./organizations";
