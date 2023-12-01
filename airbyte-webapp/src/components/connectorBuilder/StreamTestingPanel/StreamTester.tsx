3f5be566b27ad188bf69d70e5b5536adef89d417
Lake Mossman <lake@airbyte.io>
2023-12-01 14:33:22 -0800

    🪟 🎨 Adjust connector builder testing panel sizes (#10054)

--- airbyte-webapp/src/components/connectorBuilder/StreamTestingPanel/StreamTester.tsx
+++ airbyte-webapp/src/components/connectorBuilder/StreamTestingPanel/StreamTester.tsx
@@ -83,8 +83,9 @@ export const StreamTester: React.FC<{
   const hasRegularRequests =
     streamReadData !== undefined && !isError && streamReadData.slices && streamReadData.slices.length > 0;
 
-  const logsFlex = isError || errorLogs.length > 0 ? 0.75 : 0;
-  const auxiliaryRequestsFlex = hasAuxiliaryRequests && !hasRegularRequests ? 0.75 : 0;
+  const SECONDARY_PANEL_SIZE = 0.5;
+  const logsFlex = isError || errorLogs.length > 0 ? SECONDARY_PANEL_SIZE : 0;
+  const auxiliaryRequestsFlex = hasAuxiliaryRequests && !hasRegularRequests ? SECONDARY_PANEL_SIZE : 0;
 
   useEffect(() => {
     // This will only be true if the data was manually refetched by the user clicking the Test button,
