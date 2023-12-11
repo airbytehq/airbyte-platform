87c75fca57b225a130aa1d733ea9c99f68189b93
Alex Birdsall <ambirdsall@gmail.com>
2023-12-11 14:58:47 -0800

    :window: 🛠️ Make user type workspace name in confirmation modal before deleting (#9697)

--- airbyte-webapp/src/packages/cloud/views/workspaces/WorkspaceSettingsView/components/DeleteWorkspaceSection.tsx
+++ airbyte-webapp/src/packages/cloud/views/workspaces/WorkspaceSettingsView/components/DeleteWorkspaceSection.tsx
@@ -1,57 +1,25 @@
 import React from "react";
-import { FormattedMessage, useIntl } from "react-intl";
-import { useNavigate } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
 
 import { Box } from "components/ui/Box";
 import { Button } from "components/ui/Button";
 import { FlexContainer } from "components/ui/Flex";
 
+import { useConfirmWorkspaceDeletionModal } from "area/workspace/utils/useConfirmWorkspaceDeletionModal";
 import { useCurrentWorkspace } from "core/api";
 import { useRemoveCloudWorkspace } from "core/api/cloud";
-import { useConfirmationModalService } from "hooks/services/ConfirmationModal";
-import { useNotificationService } from "hooks/services/Notification";
-import { RoutePaths } from "pages/routePaths";
 
 export const DeleteWorkspaceSection: React.FC = () => {
-  const { formatMessage } = useIntl();
-  const navigate = useNavigate();
   const workspace = useCurrentWorkspace();
 
-  const { registerNotification } = useNotificationService();
-  const { openConfirmationModal, closeConfirmationModal } = useConfirmationModalService();
   const { mutateAsync: removeCloudWorkspace, isLoading: isRemovingCloudWorkspace } = useRemoveCloudWorkspace();
 
-  const deleteCurrentWorkspace = () => {
-    openConfirmationModal({
-      title: "settings.workspaceSettings.delete.confirmation.title",
-      text: "settings.workspaceSettings.delete.confirmation.text",
-      submitButtonText: "settings.workspaceSettings.delete.confirmation.submitButtonText",
-      onSubmit() {
-        closeConfirmationModal();
-        removeCloudWorkspace(workspace.workspaceId)
-          .then(() => {
-            registerNotification({
-              id: "settings.workspace.delete.success",
-              text: formatMessage({ id: "settings.workspaceSettings.delete.success" }),
-              type: "success",
-            });
-            navigate(`/${RoutePaths.Workspaces}`);
-          })
-          .catch(() => {
-            registerNotification({
-              id: "settings.workspace.delete.error",
-              text: formatMessage({ id: "settings.workspaceSettings.delete.error" }),
-              type: "error",
-            });
-          });
-      },
-    });
-  };
+  const confirmWorkspaceDeletion = useConfirmWorkspaceDeletionModal(workspace, removeCloudWorkspace);
 
   return (
     <FlexContainer justifyContent="center" alignItems="center">
       <Box p="2xl">
-        <Button isLoading={isRemovingCloudWorkspace} variant="danger" onClick={deleteCurrentWorkspace}>
+        <Button isLoading={isRemovingCloudWorkspace} variant="danger" onClick={confirmWorkspaceDeletion}>
           <FormattedMessage id="settings.workspaceSettings.deleteLabel" />
         </Button>
       </Box>
--- airbyte-webapp/src/pages/SettingsPage/components/DeleteWorkspace.tsx
+++ airbyte-webapp/src/pages/SettingsPage/components/DeleteWorkspace.tsx
@@ -1,62 +1,21 @@
-import { FormattedMessage, useIntl } from "react-intl";
-import { useNavigate } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
 
 import { Box } from "components/ui/Box";
 import { Button } from "components/ui/Button";
 import { FlexContainer } from "components/ui/Flex";
-import { Text } from "components/ui/Text";
 
+import { useConfirmWorkspaceDeletionModal } from "area/workspace/utils/useConfirmWorkspaceDeletionModal";
 import { useCurrentWorkspace, useDeleteWorkspace } from "core/api";
-import { useConfirmationModalService } from "hooks/services/ConfirmationModal";
-import { useNotificationService } from "hooks/services/Notification";
-import { RoutePaths } from "pages/routePaths";
 
 export const DeleteWorkspace: React.FC = () => {
-  const { formatMessage } = useIntl();
   const workspace = useCurrentWorkspace();
-  const navigate = useNavigate();
-  const { registerNotification } = useNotificationService();
-  const { openConfirmationModal, closeConfirmationModal } = useConfirmationModalService();
   const { mutateAsync: deleteWorkspace, isLoading: isDeletingWorkspace } = useDeleteWorkspace();
-
-  const onDeleteClick = () => {
-    openConfirmationModal({
-      title: formatMessage({ id: "settings.workspaceSettings.deleteLabel" }),
-      text: formatMessage({ id: "settings.workspaceSettings.deleteModal.proceed" }),
-      additionalContent: (
-        <Box pt="lg">
-          <Text>
-            <FormattedMessage id="settings.workspaceSettings.deleteModal.text" values={{ workspace: workspace.name }} />
-          </Text>
-        </Box>
-      ),
-      submitButtonText: formatMessage({ id: "settings.workspaceSettings.deleteModal.confirm" }),
-      onSubmit() {
-        closeConfirmationModal();
-        deleteWorkspace(workspace.workspaceId)
-          .then(() => {
-            registerNotification({
-              id: "settings.workspace.delete.success",
-              text: formatMessage({ id: "settings.workspaceSettings.delete.success" }),
-              type: "success",
-            });
-            navigate(`/${RoutePaths.Workspaces}`);
-          })
-          .catch(() => {
-            registerNotification({
-              id: "settings.workspace.delete.error",
-              text: formatMessage({ id: "settings.workspaceSettings.delete.error" }),
-              type: "error",
-            });
-          });
-      },
-    });
-  };
+  const confirmWorkspaceDeletion = useConfirmWorkspaceDeletionModal(workspace, deleteWorkspace);
 
   return (
     <FlexContainer justifyContent="center" alignItems="center">
       <Box p="xl">
-        <Button isLoading={isDeletingWorkspace} variant="danger" onClick={onDeleteClick}>
+        <Button isLoading={isDeletingWorkspace} variant="danger" onClick={confirmWorkspaceDeletion}>
           <FormattedMessage id="settings.workspaceSettings.deleteLabel" />
         </Button>
       </Box>
