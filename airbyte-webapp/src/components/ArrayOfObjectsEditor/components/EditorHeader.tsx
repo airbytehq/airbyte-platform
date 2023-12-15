8fc18ca41f614197cdee5fb68090ec63423e5c64
Vladimir <dizel852@gmail.com>
2023-12-15 22:27:47 +0200

    🪟 🔧 Remove obsolete props from `<EditorHeader />` component (#10374)

--- airbyte-webapp/src/components/ArrayOfObjectsEditor/components/EditorHeader.tsx
+++ airbyte-webapp/src/components/ArrayOfObjectsEditor/components/EditorHeader.tsx
@@ -6,40 +6,23 @@ import { Button } from "components/ui/Button";
 import { FlexContainer } from "components/ui/Flex";
 import { Text } from "components/ui/Text";
 
-import { ConnectionFormMode } from "hooks/services/ConnectionForm/ConnectionFormService";
-
 interface EditorHeaderProps {
   mainTitle?: React.ReactNode;
   addButtonText?: React.ReactNode;
   itemsCount: number;
   onAddItem: () => void;
-  /**
-   * seems like "mode" and "disabled" props can be removed since we can control fields enable/disable states on higher levels
-   * TODO: remove during ArrayOfObjectsEditor refactoring and CreateConnectionForm migration
-   */
-  mode?: ConnectionFormMode;
-  disabled?: boolean;
 }
 
-export const EditorHeader: React.FC<EditorHeaderProps> = ({
-  itemsCount,
-  onAddItem,
-  mainTitle,
-  addButtonText,
-  mode,
-  disabled,
-}) => {
+export const EditorHeader: React.FC<EditorHeaderProps> = ({ itemsCount, onAddItem, mainTitle, addButtonText }) => {
   return (
     <Box mt="sm" mb="md">
       <FlexContainer justifyContent="space-between" alignItems="center">
         <Text color="darkBlue" size="lg" bold>
           {mainTitle || <FormattedMessage id="form.items" values={{ count: itemsCount }} />}
         </Text>
-        {mode !== "readonly" && (
-          <Button variant="secondary" type="button" onClick={onAddItem} data-testid="addItemButton" disabled={disabled}>
-            {addButtonText || <FormattedMessage id="form.addItems" />}
-          </Button>
-        )}
+        <Button variant="secondary" type="button" onClick={onAddItem} data-testid="addItemButton">
+          {addButtonText || <FormattedMessage id="form.addItems" />}
+        </Button>
       </FlexContainer>
     </Box>
   );
