9411df177cad78ec9af17cda480b9c5b6bf88de2
Benoit Moriceau <benoit@airbyte.io>
2023-12-20 17:03:44 -0800

    Add labels to the connection input (#10506)

--- airbyte-commons-worker/src/main/java/io/airbyte/workers/models/CheckConnectionInput.java
+++ airbyte-commons-worker/src/main/java/io/airbyte/workers/models/CheckConnectionInput.java
@@ -7,9 +7,13 @@ package io.airbyte.workers.models;
 import io.airbyte.config.StandardCheckConnectionInput;
 import io.airbyte.persistence.job.models.IntegrationLauncherConfig;
 import io.airbyte.persistence.job.models.JobRunConfig;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * CheckConnectionInput.
@@ -22,5 +26,24 @@ public class CheckConnectionInput {
   private JobRunConfig jobRunConfig;
   private IntegrationLauncherConfig launcherConfig;
   private StandardCheckConnectionInput connectionConfiguration;
+
+  @Setter(AccessLevel.NONE)
+  private Map<String, String> labels = new HashMap<>();
+
+  public CheckConnectionInput(JobRunConfig jobRunConfig,
+                              IntegrationLauncherConfig launcherConfig,
+                              StandardCheckConnectionInput connectionConfiguration) {
+    this.jobRunConfig = jobRunConfig;
+    this.launcherConfig = launcherConfig;
+    this.connectionConfiguration = connectionConfiguration;
+  }
+
+  public void addLabel(final String key, final String value) {
+    labels.put(key, value);
+  }
+
+  public void addLabels(final Map<String, String> labels) {
+    this.labels.putAll(labels);
+  }
 
 }
