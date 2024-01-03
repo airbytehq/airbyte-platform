ce08e690e49a4183724cbab25125fb9fd072f3e5
Ryan Br <ryan.broughan@gmail.com>
2024-01-03 13:23:07 -0800

    Shorten job id for check. (#10535)

--- airbyte-commons-worker/src/main/kotlin/io/airbyte/workers/workload/WorkloadIdGenerator.kt
+++ airbyte-commons-worker/src/main/kotlin/io/airbyte/workers/workload/WorkloadIdGenerator.kt
@@ -13,15 +13,19 @@ import java.util.UUID
  *     <code>&lt;the connection ID&gtl;_&lt;the job ID&gt;_&lt;the job attempt number&gt;_&lt;the workload job type&gt;</code>
  * </p>
  * where the workload type is a value defined in {@link WorkloadType}.
+ *
+ * IMPORTANT: Kube only accepts labels of up to 63 characters, so we try to keep these short for use with kube.
+ * NOTE: UUIDs are 36 characters.
  */
 @Singleton
 class WorkloadIdGenerator {
   fun generateCheckWorkloadId(
-    actorId: UUID,
+    actorDefinitionId: UUID,
     jobId: String,
     attemptNumber: Int,
   ): String {
-    return "${actorId}_${jobId}_${attemptNumber}_check"
+    val truncatedJobId = jobId.take(8)
+    return "${actorDefinitionId}_${truncatedJobId}_${attemptNumber}_check"
   }
 
   fun generateDiscoverWorkloadId(
