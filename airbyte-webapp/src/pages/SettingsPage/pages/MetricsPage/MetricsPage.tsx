10bca177ff423fca5cbc581608cad158da673c4b
Vladimir <dizel852@gmail.com>
2024-01-08 14:27:19 +0200

    🪟 🔧 Update `<MetricsPage />` (#10529)

--- airbyte-webapp/src/pages/SettingsPage/pages/MetricsPage/MetricsPage.tsx
+++ airbyte-webapp/src/pages/SettingsPage/pages/MetricsPage/MetricsPage.tsx
@@ -5,33 +5,19 @@ import { HeadTitle } from "components/common/HeadTitle";
 import { Box } from "components/ui/Box";
 import { Card } from "components/ui/Card";
 
-import { useCurrentWorkspace } from "core/api";
 import { useTrackPage, PageTrackingCodes } from "core/services/analytics";
 
-import MetricsForm from "./components/MetricsForm";
-import useWorkspaceEditor from "../../components/useWorkspaceEditor";
+import { MetricsForm } from "./components/MetricsForm";
 
 export const MetricsPage: React.FC = () => {
-  const workspace = useCurrentWorkspace();
-  const { errorMessage, successMessage, loading, updateData } = useWorkspaceEditor();
-
   useTrackPage(PageTrackingCodes.SETTINGS_METRICS);
-  const onChange = async (data: { anonymousDataCollection: boolean }) => {
-    await updateData({ ...workspace, ...data, news: !!workspace.news, securityUpdates: !!workspace.securityUpdates });
-  };
 
   return (
     <>
       <HeadTitle titles={[{ id: "sidebar.settings" }, { id: "settings.metrics" }]} />
       <Card title={<FormattedMessage id="settings.metricsSettings" />}>
         <Box p="xl">
-          <MetricsForm
-            onChange={onChange}
-            anonymousDataCollection={workspace.anonymousDataCollection}
-            successMessage={successMessage}
-            errorMessage={errorMessage}
-            isLoading={loading}
-          />
+          <MetricsForm />
         </Box>
       </Card>
     </>
