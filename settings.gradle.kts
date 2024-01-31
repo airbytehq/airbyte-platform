e4a23bc659ea1867fbc219f79281690bc503a85b
perangel <angel@airbyte.io>
2024-01-31 09:56:40 -0500

    fix: don't upload gradle build scans in the background in CI (#10979)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,17 +35,17 @@ gradleEnterprise {
     }
 }
 
+val isCiServer = System.getenv().containsKey("CI")
+
 gradleEnterprise {
     buildScan {
+        isUploadInBackground = !isCiServer // Disable async upload so that the containers doesn't terminate the upload
         buildScanPublished {
             file("scan-journal.log").writeText("${java.util.Date()} - $buildScanId - ${buildScanUri}\n")
         }
     }
 }
 
-
-val isCiServer = System.getenv().containsKey("CI")
-//
 buildCache {
     // we use a different caching mechanism for Dagger builds
     if (System.getenv("DAGGER") == null) {
