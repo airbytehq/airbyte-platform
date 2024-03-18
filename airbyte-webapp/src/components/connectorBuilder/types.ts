bf465092361c9672ffc1157199d31dd1f7eca045
Lake Mossman <lake@airbyte.io>
2024-03-18 14:27:14 -0700

    🪟 🎉 Add common formats to suggested datetime format list (#11613)

--- airbyte-webapp/src/components/connectorBuilder/types.ts
+++ airbyte-webapp/src/components/connectorBuilder/types.ts
@@ -298,8 +298,10 @@ export const SMALL_DURATION_OPTIONS = [
 ];
 
 export const DATETIME_FORMAT_OPTIONS = [
+  { value: "%s" },
   { value: "%Y-%m-%d" },
   { value: "%Y-%m-%d %H:%M:%S" },
+  { value: "%Y-%m-%dT%H:%M:%SZ" },
   { value: "%Y-%m-%d %H:%M:%S.%f+00:00" },
   { value: "%Y-%m-%dT%H:%M:%S.%f%z" },
 ];
