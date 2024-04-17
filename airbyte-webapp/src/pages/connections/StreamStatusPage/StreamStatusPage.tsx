da36da88779fbce8ca5e8cee8b27bee1f4b11666
Vladimir <dizel852@gmail.com>
2024-04-17 14:42:21 +0300

    🪟 🎉  New connection header CTAs (#11981)
    Co-authored-by: Chandler Prall <chandler.prall@gmail.com>

--- airbyte-webapp/src/pages/connections/StreamStatusPage/StreamStatusPage.tsx
+++ airbyte-webapp/src/pages/connections/StreamStatusPage/StreamStatusPage.tsx
@@ -1,16 +1,28 @@
 import { ConnectionSyncContextProvider } from "components/connection/ConnectionSync/ConnectionSyncContext";
 import { FlexContainer } from "components/ui/Flex";
 
+import { useExperiment } from "hooks/services/Experiment";
+
 import { ConnectionStatusCard } from "./ConnectionStatusCard";
+import { ConnectionStatusMessages } from "./ConnectionStatusMessages";
+import { ConnectionSyncStatusCard } from "./ConnectionSyncStatusCard";
 import { StreamsList } from "./StreamsList";
 import { StreamsListContextProvider } from "./StreamsListContext";
 
 export const StreamStatusPage = () => {
+  const isSimplifiedCreation = useExperiment("connection.simplifiedCreation", false);
   return (
     <ConnectionSyncContextProvider>
       <StreamsListContextProvider>
         <FlexContainer direction="column" gap="md">
-          <ConnectionStatusCard />
+          {isSimplifiedCreation ? (
+            <>
+              <ConnectionStatusMessages />
+              <ConnectionSyncStatusCard />
+            </>
+          ) : (
+            <ConnectionStatusCard />
+          )}
           <StreamsList />
         </FlexContainer>
       </StreamsListContextProvider>
