9f3023014ffb9d7576b19053d00447b0ebbe5c3f
Pedro S. Lopez <pedroslopez@me.com>
2024-04-29 16:57:05 -0400

    remove required organizationId from CloudWorkspaceRead (#12292)

--- airbyte-webapp/src/packages/cloud/views/billing/BillingPage/components/BillingBanners.test.tsx
+++ airbyte-webapp/src/packages/cloud/views/billing/BillingPage/components/BillingBanners.test.tsx
@@ -46,7 +46,6 @@ function mockWorkspace(
 ) {
   mocked(useGetCloudWorkspace).mockReturnValue({
     workspaceId: "123",
-    organizationId: "aa00cf13-8998-4b05-8616-3b5fcd2f4c5c",
     remainingCredits: credits,
     creditStatus,
     workspaceTrialStatus: trialStatus,
