6c2a0cc7b81552999327eb66c80d130992081853
Lake Mossman <lake@airbyte.io>
2024-05-01 10:43:51 -0700

    🪟 🔧 (Second try) Cloud E2E smoke test for Connector Builder (#12313)
    Co-authored-by: Teal Larson <LARSON.TEAL@GMAIL.COM>

--- airbyte-webapp/cypress/cypress.cloud-config.ts
+++ airbyte-webapp/cypress/cypress.cloud-config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  viewportHeight: 800,
+  viewportWidth: 1280,
+  pageLoadTimeout: 10000,
   e2e: {
     baseUrl: "https://localhost:3001/",
     specPattern: ["cypress/cloud-e2e/**/*.cy.ts"],
     supportFile: "cypress/support/cloud-e2e.ts",
+    env: {
+      LOGIN_URL: "https://frontend-dev-cloud.airbyte.com",
+    },
   },
 });
