d4dd6f2e273f3942df50291d648799bb3f763ddf
Jonathan Pearlin <jonathan@airbyte.io>
2024-05-06 09:36:24 -0400

    Fix custom JsonNode serde (#12335)

--- airbyte-api/src/main/kotlin/client/infrastructure/JsonNodeAdapter.kt
+++ airbyte-api/src/main/kotlin/client/infrastructure/JsonNodeAdapter.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2020-2024 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.api.client.infrastructure
 
 import com.fasterxml.jackson.databind.JsonNode
@@ -5,14 +9,23 @@ import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 import io.airbyte.commons.json.Jsons
 
+/**
+ * Custom Moshi adapter that handles the use of [JsonNode]
+ * that is used throughout the OpenAPI specification.
+ * <p />
+ * This adapter ensures that strings can be deserialized into [JsonNode]
+ * fields and that [JsonNode] fields in the model objects will be
+ * serialized into String representations of the JSON data.
+ */
 class JsonNodeAdapter {
+  @Suppress("UNCHECKED_CAST")
   @ToJson
-  fun toJson(value: JsonNode): String {
-    return Jsons.serialize(value)
+  fun toJson(value: JsonNode): Map<String, Any> {
+    return Jsons.`object`(value, Map::class.java) as Map<String, Any>
   }
 
   @FromJson
-  fun fromJson(value: String): JsonNode {
-    return Jsons.deserialize(value)
+  fun fromJson(value: Map<String, Any>): JsonNode {
+    return Jsons.jsonNode(value)
   }
 }
--- airbyte-api/src/test/kotlin/client/infrastructure/JsonNodeAdapterTest.kt
+++ airbyte-api/src/test/kotlin/client/infrastructure/JsonNodeAdapterTest.kt
@@ -1,24 +1,129 @@
-package client.infrastructure
+/*
+ * Copyright (c) 2020-2024 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.api.client.infrastructure
 
 import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.node.JsonNodeFactory
-import com.fasterxml.jackson.databind.node.TextNode
-import io.airbyte.api.client.infrastructure.JsonNodeAdapter
+import com.squareup.moshi.adapter
+import io.airbyte.api.client2.model.generated.AirbyteCatalog
+import io.airbyte.api.client2.model.generated.AirbyteStream
+import io.airbyte.api.client2.model.generated.AirbyteStreamAndConfiguration
+import io.airbyte.api.client2.model.generated.AirbyteStreamConfiguration
+import io.airbyte.api.client2.model.generated.ConnectionRead
+import io.airbyte.api.client2.model.generated.ConnectionReadList
+import io.airbyte.api.client2.model.generated.ConnectionStatus
+import io.airbyte.api.client2.model.generated.DestinationSyncMode
+import io.airbyte.api.client2.model.generated.SyncMode
+import io.airbyte.commons.json.Jsons
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.openapitools.client.infrastructure.Serializer
+import java.util.UUID
 
-class JsonNodeAdapterTest {
-  val jsonNode: JsonNode = JsonNodeFactory.instance.objectNode().set("key1", TextNode.valueOf("value1"))
-  val jsonString = "{\"key1\":\"value1\"}"
-  val adapter = JsonNodeAdapter()
+internal class JsonNodeAdapterTest {
+  private lateinit var data: Map<String, Any>
+  private lateinit var jsonNode: JsonNode
+  private lateinit var jsonString: String
+  private lateinit var adapter: JsonNodeAdapter
 
-  @Test
-  fun toJson() {
-    assertEquals(jsonString, adapter.toJson(jsonNode))
+  @BeforeEach
+  internal fun setup() {
+    data = mapOf("key1" to "value1")
+    jsonNode = Jsons.jsonNode(data)
+    jsonString = Jsons.serialize(data)
+    adapter = JsonNodeAdapter()
   }
 
   @Test
-  fun fromJson() {
-    assertEquals(jsonNode, adapter.fromJson(jsonString))
+  internal fun toJson() {
+    assertEquals(data, adapter.toJson(jsonNode))
+  }
+
+  @Test
+  internal fun fromJson() {
+    assertEquals(jsonNode, adapter.fromJson(data))
+  }
+
+  @Test
+  @OptIn(ExperimentalStdlibApi::class)
+  internal fun testDeserialization() {
+    assertEquals(jsonNode, Serializer.moshi.adapter<JsonNode>().fromJson(jsonString))
+  }
+
+  @Test
+  @OptIn(ExperimentalStdlibApi::class)
+  internal fun testSerialization() {
+    assertEquals(jsonString, Serializer.moshi.adapter<JsonNode>().toJson(jsonNode))
+  }
+
+  @Test
+  @OptIn(ExperimentalStdlibApi::class)
+  internal fun testSerdeGeneratedModelObject() {
+    val connectionRead =
+      ConnectionRead(
+        connectionId = UUID.randomUUID(),
+        sourceId = UUID.randomUUID(),
+        destinationId = UUID.randomUUID(),
+        syncCatalog =
+          AirbyteCatalog(
+            listOf(
+              AirbyteStreamAndConfiguration(
+                stream =
+                  AirbyteStream(
+                    jsonSchema = jsonNode,
+                    name = "stream",
+                  ),
+                config =
+                  AirbyteStreamConfiguration(
+                    syncMode = SyncMode.INCREMENTAL,
+                    destinationSyncMode = DestinationSyncMode.APPEND,
+                  ),
+              ),
+            ),
+          ),
+        status = ConnectionStatus.ACTIVE,
+        name = "name",
+        breakingChange = false,
+      )
+    val connectionReadList = ConnectionReadList(listOf(connectionRead))
+    val adapter = Serializer.moshi.adapter<ConnectionReadList>()
+    val json = adapter.toJson(connectionReadList)
+    assertEquals(connectionReadList, adapter.fromJson(json))
+  }
+
+  @Test
+  @OptIn(ExperimentalStdlibApi::class)
+  internal fun testSerdeGeneratedArrayOfJsonNodes() {
+    val resp =
+      TestResponse(
+        sampleArray = listOf(Jsons.jsonNode(mapOf("foo" to "bar"))),
+        otherField = 5,
+      )
+    val adapter = Serializer.moshi.adapter<TestResponse>()
+    val json = adapter.toJson(resp)
+    println(json)
+    assertEquals(resp, adapter.fromJson(json))
+  }
+
+  internal class TestResponse(val sampleArray: List<JsonNode>, val otherField: Int) {
+    override fun equals(other: Any?): Boolean {
+      if (this === other) return true
+      if (javaClass != other?.javaClass) return false
+
+      other as TestResponse
+
+      if (sampleArray != other.sampleArray) return false
+      if (otherField != other.otherField) return false
+
+      return true
+    }
+
+    override fun hashCode(): Int {
+      var result = sampleArray.hashCode()
+      result = 31 * result + otherField
+      return result
+    }
   }
 }
