54be8877404a1d9e77b29dd5be2b2a1086170b11
perangel <angel@airbyte.io>
2024-05-13 15:42:31 -0400

    fix(helm): external database support with secrets-backed credentials (#12423)

--- charts/helm-tests/tests/enterprise_config_test.go
+++ charts/helm-tests/tests/enterprise_config_test.go
@@ -12,14 +12,44 @@ import (
 )
 
 func TestBasicEnterpriseConfigWithHelmValues(t *testing.T) {
+	t.Run("require global.enterprise.secretName", func(t *testing.T) {
+		helmOpts := baseHelmOptions()
+		helmOpts.SetValues["global.edition"] = "enterprise"
+		// set to empty string since it has a default value
+		helmOpts.SetValues["global.enterprise.secretName"] = ""
+		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Ocvatia"
+		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
+		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
+		require.ErrorContains(t, err, "You must set `global.enterprise.secretName` when `global.edition` is 'enterprise'")
+	})
+
+	t.Run("require global.enterprise.licenseKeySecretKey", func(t *testing.T) {
+		helmOpts := baseHelmOptions()
+		helmOpts.SetValues["global.edition"] = "enterprise"
+		// set to empty string since it has a default value
+		helmOpts.SetValues["global.enterprise.licenseKeySecretKey"] = ""
+		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Ocvatia"
+		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
+		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
+		require.ErrorContains(t, err, "You must set `global.enterprise.licenseKeySecretKey` when `global.edition` is 'enterprise'")
+	})
+
+	t.Run("require global.auth.instanceAdmin.secretName", func(t *testing.T) {
+		helmOpts := baseHelmOptions()
+		helmOpts.SetValues["global.edition"] = "enterprise"
+		// set to empty string since it has a default value
+		helmOpts.SetValues["global.auth.instanceAdmin.secretName"] = ""
+		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Ocvatia"
+		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
+		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
+		require.ErrorContains(t, err, "You must set `global.auth.instanceAdmin.secretName` when `global.edition` is 'enterprise'")
+	})
+
 	t.Run("require global.auth.instanceAdmin.firstName", func(t *testing.T) {
 		helmOpts := baseHelmOptions()
 		helmOpts.SetValues["global.edition"] = "enterprise"
-		helmOpts.SetValues["global.enterprise.licenseKeySecretName"] = "airbyte-license"
 		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = ""
 		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
-		helmOpts.SetValues["global.auth.instanceAdmin.passwordSecretName"] = "airbyte-admin"
-		helmOpts.SetValues["global.auth.instanceAdmin.emailSecretName"] = "airbyte-admin"
 		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
 		require.ErrorContains(t, err, "You must set `global.auth.instanceAdmin.firstName` when `global.edition` is 'enterprise'")
 	})
@@ -27,50 +57,128 @@ func TestBasicEnterpriseConfigWithHelmValues(t *testing.T) {
 	t.Run("require global.auth.instanceAdmin.lastName", func(t *testing.T) {
 		helmOpts := baseHelmOptions()
 		helmOpts.SetValues["global.edition"] = "enterprise"
-		helmOpts.SetValues["global.enterprise.licenseKeySecretName"] = "airbyte-license"
 		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Octavia"
 		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = ""
-		helmOpts.SetValues["global.auth.instanceAdmin.passwordSecretName"] = "airbyte-admin"
-		helmOpts.SetValues["global.auth.instanceAdmin.emailSecretName"] = "airbyte-admin"
 		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
 		require.ErrorContains(t, err, "You must set `global.auth.instanceAdmin.lastName` when `global.edition` is 'enterprise'")
 	})
 
-	t.Run("require global.auth.instanceAdmin.emailSecretName", func(t *testing.T) {
+	t.Run("require global.auth.instanceAdmin.emailSecretKey", func(t *testing.T) {
 		helmOpts := baseHelmOptions()
 		helmOpts.SetValues["global.edition"] = "enterprise"
-		helmOpts.SetValues["global.enterprise.licenseKeyExistingSecret"] = "airbyte-license"
 		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Octavia"
 		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
-		helmOpts.SetValues["global.auth.instanceAdmin.emailSecretName"] = ""
-		helmOpts.SetValues["global.auth.instanceAdmin.passwordSecretName"] = "airbyte-admin"
+		// set to empty string since it has a default value
+		helmOpts.SetValues["global.auth.instanceAdmin.emailSecretKey"] = ""
 		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
-		require.ErrorContains(t, err, "You must set `global.auth.instanceAdmin.emailSecretName` when `global.edition` is 'enterprise'")
+		require.ErrorContains(t, err, "You must set `global.auth.instanceAdmin.emailSecretKey` when `global.edition` is 'enterprise'")
 	})
 
-	t.Run("require global.auth.instanceAdmin.passwordSecretName", func(t *testing.T) {
+	t.Run("require global.auth.instanceAdmin.passwordSecretKey", func(t *testing.T) {
 		helmOpts := baseHelmOptions()
 		helmOpts.SetValues["global.edition"] = "enterprise"
-		helmOpts.SetValues["global.enterprise.licenseKeyExistingSecret"] = "airbyte-license"
 		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Octavia"
 		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
-		helmOpts.SetValues["global.auth.instanceAdmin.emailSecretName"] = "airbyte-admin"
-		helmOpts.SetValues["global.auth.instanceAdmin.passwordSecretName"] = ""
+		// set to empty string since it has a default value
+		helmOpts.SetValues["global.auth.instanceAdmin.passwordSecretKey"] = ""
 		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
-		require.ErrorContains(t, err, "You must set `global.auth.instanceAdmin.passwordSecretName` when `global.edition` is 'enterprise'")
+		require.ErrorContains(t, err, "You must set `global.auth.instanceAdmin.passwordSecretKey` when `global.edition` is 'enterprise'")
+	})
+
+	t.Run("require global.auth.identityProvider.secretName when enabling SSO", func(t *testing.T) {
+		helmOpts := baseHelmOptions()
+		helmOpts.SetValues["global.edition"] = "enterprise"
+		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Octavia"
+		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
+		helmOpts.SetValues["global.auth.identityProvider.secretName"] = ""
+		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
+		require.ErrorContains(t, err, "You must set `global.auth.identityProvider.secretName` when enabling SSO")
+	})
+
+	t.Run("require global.auth.identityProvider.type when enabling SSO", func(t *testing.T) {
+		helmOpts := baseHelmOptions()
+		helmOpts.SetValues["global.edition"] = "enterprise"
+		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Octavia"
+		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
+		helmOpts.SetValues["global.auth.identityProvider.secretName"] = "sso-secrets"
+		helmOpts.SetValues["global.auth.identityProvider.type"] = ""
+		helmOpts.SetValues["global.auth.identityProvider.oidc.domain"] = "sso.example.com"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.appName"] = "example-app"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.clientIdSecretKey"] = "client-id"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.clientSecretSecretKey"] = "client-secret"
+		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
+		require.ErrorContains(t, err, "You must set `global.auth.identityProvider.type` when enabling SSO")
+	})
+
+	t.Run("require global.auth.identityProvider.oidc.domain when enabling SSO", func(t *testing.T) {
+		helmOpts := baseHelmOptions()
+		helmOpts.SetValues["global.edition"] = "enterprise"
+		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Octavia"
+		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
+		helmOpts.SetValues["global.auth.identityProvider.secretName"] = "sso-secrets"
+		helmOpts.SetValues["global.auth.identityProvider.type"] = "oidc"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.domain"] = ""
+		helmOpts.SetValues["global.auth.identityProvider.oidc.appName"] = "example-app"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.clientIdSecretKey"] = "client-id"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.clientSecretSecretKey"] = "client-secret"
+		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
+		require.ErrorContains(t, err, "You must set `global.auth.identityProvider.oidc.domain` when enabling SSO")
+	})
+
+	t.Run("require global.auth.identityProvider.oidc.appName when enabling SSO", func(t *testing.T) {
+		helmOpts := baseHelmOptions()
+		helmOpts.SetValues["global.edition"] = "enterprise"
+		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Octavia"
+		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
+		helmOpts.SetValues["global.auth.identityProvider.secretName"] = "sso-secrets"
+		helmOpts.SetValues["global.auth.identityProvider.type"] = "oidc"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.domain"] = "sso.example.com"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.appName"] = ""
+		helmOpts.SetValues["global.auth.identityProvider.oidc.clientIdSecretKey"] = "client-id"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.clientSecretSecretKey"] = "client-secret"
+		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
+		require.ErrorContains(t, err, "You must set `global.auth.identityProvider.oidc.appName` when enabling SSO")
+	})
+
+	t.Run("require global.auth.identityProvider.oidc.clientIdSecretKey when enabling SSO", func(t *testing.T) {
+		helmOpts := baseHelmOptions()
+		helmOpts.SetValues["global.edition"] = "enterprise"
+		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Octavia"
+		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
+		helmOpts.SetValues["global.auth.identityProvider.secretName"] = "sso-secrets"
+		helmOpts.SetValues["global.auth.identityProvider.type"] = "oidc"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.domain"] = "sso.example.com"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.appName"] = "example-app"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.clientIdSecretKey"] = ""
+		helmOpts.SetValues["global.auth.identityProvider.oidc.clientSecretSecretKey"] = "client-secret"
+		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
+		require.ErrorContains(t, err, "You must set `global.auth.identityProvider.oidc.clientIdSecretKey` when enabling SSO")
+	})
+
+	t.Run("require global.auth.identityProvider.oidc.clientSecretSecretKey when enabling SSO", func(t *testing.T) {
+		helmOpts := baseHelmOptions()
+		helmOpts.SetValues["global.edition"] = "enterprise"
+		helmOpts.SetValues["global.auth.instanceAdmin.firstName"] = "Octavia"
+		helmOpts.SetValues["global.auth.instanceAdmin.lastName"] = "Squidington"
+		helmOpts.SetValues["global.auth.identityProvider.secretName"] = "sso-secrets"
+		helmOpts.SetValues["global.auth.identityProvider.type"] = "oidc"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.domain"] = "sso.example.com"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.appName"] = "example-app"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.clientIdSecretKey"] = "client-id"
+		helmOpts.SetValues["global.auth.identityProvider.oidc.clientSecretSecretKey"] = ""
+		_, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
+		require.ErrorContains(t, err, "You must set `global.auth.identityProvider.oidc.clientSecretSecretKey` when enabling SSO")
 	})
 
 	t.Run("should set enterprise config env vars", func(t *testing.T) {
 		helmOpts := baseHelmOptionsForEnterpriseWithValues()
-		helmOpts.SetValues["global.enterprise.licenseKeySecretName"] = "airbyte-license"
-		helmOpts.SetValues["global.auth.instanceAdmin.emailSecretName"] = "sso-secrets"
-		helmOpts.SetValues["global.auth.instanceAdmin.passwordSecretName"] = "sso-secrets"
-		helmOpts.SetValues["global.auth.identityProvider.type"] = "okta"
+		helmOpts.SetValues["global.enterprise.secretName"] = "airbyte-license"
+		helmOpts.SetValues["global.auth.instanceAdmin.secretName"] = "sso-secrets"
+		helmOpts.SetValues["global.auth.identityProvider.secretName"] = "sso-secrets"
+		helmOpts.SetValues["global.auth.identityProvider.type"] = "oidc"
 		helmOpts.SetValues["global.auth.identityProvider.oidc.domain"] = "sso.example.org"
 		helmOpts.SetValues["global.auth.identityProvider.oidc.appName"] = "sso-app"
-		helmOpts.SetValues["global.auth.identityProvider.oidc.clientIdSecretName"] = "sso-secrets"
 		helmOpts.SetValues["global.auth.identityProvider.oidc.clientIdSecretKey"] = "client-id"
-		helmOpts.SetValues["global.auth.identityProvider.oidc.clientSecretSecretName"] = "sso-secrets"
 		helmOpts.SetValues["global.auth.identityProvider.oidc.clientSecretSecretKey"] = "client-secret"
 
 		chartYaml, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
@@ -82,8 +190,8 @@ func TestBasicEnterpriseConfigWithHelmValues(t *testing.T) {
 				"AIRBYTE_URL":             expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("AIRBYTE_URL"),
 				"INITIAL_USER_FIRST_NAME": expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("INITIAL_USER_FIRST_NAME"),
 				"INITIAL_USER_LAST_NAME":  expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("INITIAL_USER_LAST_NAME"),
-				"INITIAL_USER_EMAIL":      expectedSecretVar().RefName("sso-secrets").RefKey("initial-user-email"),
-				"INITIAL_USER_PASSWORD":   expectedSecretVar().RefName("sso-secrets").RefKey("initial-user-password"),
+				"INITIAL_USER_EMAIL":      expectedSecretVar().RefName("sso-secrets").RefKey("instance-admin-email"),
+				"INITIAL_USER_PASSWORD":   expectedSecretVar().RefName("sso-secrets").RefKey("instance-admin-password"),
 				"IDENTITY_PROVIDER_TYPE":  expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("IDENTITY_PROVIDER_TYPE"),
 				"OIDC_DOMAIN":             expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("OIDC_DOMAIN"),
 				"OIDC_APP_NAME":           expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("OIDC_APP_NAME"),
@@ -110,8 +218,8 @@ func TestBasicEnterpriseConfigWithHelmValues(t *testing.T) {
 				"AIRBYTE_URL":             expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("AIRBYTE_URL"),
 				"INITIAL_USER_FIRST_NAME": expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("INITIAL_USER_FIRST_NAME"),
 				"INITIAL_USER_LAST_NAME":  expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("INITIAL_USER_LAST_NAME"),
-				"INITIAL_USER_EMAIL":      expectedSecretVar().RefName("sso-secrets").RefKey("initial-user-email"),
-				"INITIAL_USER_PASSWORD":   expectedSecretVar().RefName("sso-secrets").RefKey("initial-user-password"),
+				"INITIAL_USER_EMAIL":      expectedSecretVar().RefName("sso-secrets").RefKey("instance-admin-email"),
+				"INITIAL_USER_PASSWORD":   expectedSecretVar().RefName("sso-secrets").RefKey("instance-admin-password"),
 				"IDENTITY_PROVIDER_TYPE":  expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("IDENTITY_PROVIDER_TYPE"),
 				"OIDC_DOMAIN":             expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("OIDC_DOMAIN"),
 				"OIDC_APP_NAME":           expectedConfigMapVar().RefName("airbyte-airbyte-env").RefKey("OIDC_APP_NAME"),
@@ -131,28 +239,5 @@ func TestBasicEnterpriseConfigWithHelmValues(t *testing.T) {
 				verifyEnvVar(t, expected, actual)
 			}
 		})
-
-	})
-
-	t.Run("should require a licenseKeySecretName when edition is enterprise", func(t *testing.T) {
-		helmOpts := baseHelmOptionsForEnterpriseWithValues()
-		helmOpts.SetValues["global.enterprise.licenseKeySecretName"] = "airbyte-license"
-
-		chartYaml, err := helm.RenderTemplateE(t, helmOpts, chartPath, "airbyte", nil)
-		require.NotNil(t, chartYaml)
-		require.NoError(t, err)
-
-		// Verify that the airbyte server deployment has the correct vars
-		airbyteServerDep, err := getDeployment(chartYaml, "airbyte-server")
-		expectedEnvVarKeys := map[string]expectedEnvVar{
-			"AIRBYTE_LICENSE_KEY": expectedSecretVar().RefName("airbyte-license").RefKey("license-key"),
-		}
-
-		airbyteServerEnvVars := envVarMap(airbyteServerDep.Spec.Template.Spec.Containers[0].Env)
-		for k, expected := range expectedEnvVarKeys {
-			actual, ok := airbyteServerEnvVars[k]
-			assert.True(t, ok, fmt.Sprintf("`%s` should be declared as an environment variable", k))
-			verifyEnvVar(t, expected, actual)
-		}
 	})
 }
