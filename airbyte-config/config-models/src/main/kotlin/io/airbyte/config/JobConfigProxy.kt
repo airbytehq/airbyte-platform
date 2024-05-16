7fccf743f5ede1b0ad1a5ea75a1a4d336a16a8de
Jimmy Ma <gosusnp@users.noreply.github.com>
2024-05-16 11:10:56 -0700

    feat: Add refresh_types to sync event for refreshes (#12440)

--- airbyte-config/config-models/src/main/kotlin/io/airbyte/config/JobConfigProxy.kt
+++ airbyte-config/config-models/src/main/kotlin/io/airbyte/config/JobConfigProxy.kt
@@ -1,11 +1,15 @@
 package io.airbyte.config
 
+import io.airbyte.config.JobConfig.ConfigType
 import io.airbyte.protocol.models.ConfiguredAirbyteCatalog
 
 /**
  * JobConfig proxy that abstracts some complexity from the underlying union-like representation.
  */
 class JobConfigProxy(private val jobConfig: JobConfig) {
+  val configType: ConfigType
+    get() = jobConfig.configType
+
   val configuredCatalog: ConfiguredAirbyteCatalog?
     get() =
       when (jobConfig.configType) {
