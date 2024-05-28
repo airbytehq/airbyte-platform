f2bdfc45f4f6575a7b8e927c76283529432fce2c
Jonathan Pearlin <jonathan@airbyte.io>
2024-05-28 11:06:02 -0400

    Configure Micronaut ObjectMapper (#12586)

--- airbyte-commons-micronaut/build.gradle.kts
+++ airbyte-commons-micronaut/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
   id("io.airbyte.gradle.jvm.lib")
   id("io.airbyte.gradle.publish")
+  kotlin("jvm")
+  kotlin("kapt")
 }
 
 dependencies {
@@ -9,8 +11,12 @@ dependencies {
   annotationProcessor(platform(libs.micronaut.platform))
   annotationProcessor(libs.bundles.micronaut.annotation.processor)
 
+  kapt(platform(libs.micronaut.platform))
+  kapt(libs.bundles.micronaut.annotation.processor)
+
   implementation(platform(libs.micronaut.platform))
   implementation(libs.bundles.micronaut)
+  implementation(libs.bundles.micronaut.annotation)
   implementation(libs.micronaut.security)
 
   implementation(project(":airbyte-commons"))
@@ -25,4 +31,13 @@ dependencies {
 
 tasks.named<Test>("test") {
   maxHeapSize = "2g"
+}
+
+// The DuplicatesStrategy will be required while this module is mixture of kotlin and java _with_ lombok dependencies.)
+// Kapt, by default, runs all annotation(processors and disables annotation(processing by javac, however)
+// this default behavior(breaks the lombok java annotation(processor.  To avoid(lombok breaking, kapt(has)
+// keepJavacAnnotationProcessors enabled, which causes duplicate META-INF files to be generated.)
+// Once lombok has been removed, this can also be removed.)
+tasks.withType<Jar>().configureEach {
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
