6827a42ff192088d46c144f7f87f8b2ca2017759
Teal Larson <LARSON.TEAL@GMAIL.COM>
2024-06-10 15:38:21 -0400

    fix: correctly display connection-level stats during a sync (flagged) (#12742)

--- airbyte-webapp/src/pages/connections/StreamStatusPage/StreamsListSubtitle.tsx
+++ airbyte-webapp/src/pages/connections/StreamStatusPage/StreamsListSubtitle.tsx
@@ -30,15 +30,15 @@ export const StreamsListSubtitle: React.FC<StreamsListSubtitleProps> = ({
         nextSync && (
           <FormattedMessage id="connection.stream.status.nextTry" values={{ sync: dayjs(nextSync).fromNow() }} />
         )}
-      {(showSyncProgress && connectionStatus === ConnectionStatusIndicatorStatus.Syncing) ||
-        (connectionStatus === ConnectionStatusIndicatorStatus.Queued &&
-          (recordsLoaded ? (
-            <FormattedMessage id="sources.countRecordsLoaded" values={{ count: recordsLoaded }} />
-          ) : recordsExtracted ? (
-            <FormattedMessage id="sources.countRecordsExtracted" values={{ count: recordsExtracted }} />
-          ) : (
-            <FormattedMessage id="connection.stream.status.table.syncStarting" />
-          )))}
+      {((showSyncProgress && connectionStatus === ConnectionStatusIndicatorStatus.Syncing) ||
+        connectionStatus === ConnectionStatusIndicatorStatus.Queued) &&
+        (recordsLoaded ? (
+          <FormattedMessage id="sources.countRecordsLoaded" values={{ count: recordsLoaded }} />
+        ) : recordsExtracted ? (
+          <FormattedMessage id="sources.countRecordsExtracted" values={{ count: recordsExtracted }} />
+        ) : (
+          <FormattedMessage id="connection.stream.status.table.syncStarting" />
+        ))}
     </Text>
   );
 };
