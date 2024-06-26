airbytehq/airbyte-platform

commit 986253c7c2ae834d2dfc482b5067baeac401d0bd

refactor: use Kotlin-generated API client/model in airbyte-worker, workload-launcher and container-orchestrator (#12590)
Co-authored-by: jonsspaghetti <jon@airbyte.io>
Co-authored-by: Jimmy Ma <gosusnp@users.noreply.github.com>

airbyte-api-server/src/main/kotlin/io/airbyte/api/server/mappers/ConnectionUpdateMapper.kt
@@ -6,13 +6,13 @@ package io.airbyte.api.server.mappers
 
 import io.airbyte.airbyte_api.model.generated.ConnectionPatchRequest
 import io.airbyte.airbyte_api.model.generated.ScheduleTypeEnum
-import io.airbyte.api.client.model.generated.AirbyteCatalog
-import io.airbyte.api.client.model.generated.ConnectionScheduleData
-import io.airbyte.api.client.model.generated.ConnectionScheduleDataCron
-import io.airbyte.api.client.model.generated.ConnectionScheduleType
-import io.airbyte.api.client.model.generated.ConnectionStatus
-import io.airbyte.api.client.model.generated.ConnectionUpdate
-import io.airbyte.api.client.model.generated.Geography
+import io.airbyte.api.client2.model.generated.AirbyteCatalog
+import io.airbyte.api.client2.model.generated.ConnectionScheduleData
+import io.airbyte.api.client2.model.generated.ConnectionScheduleDataCron
+import io.airbyte.api.client2.model.generated.ConnectionScheduleType
+import io.airbyte.api.client2.model.generated.ConnectionStatus
+import io.airbyte.api.client2.model.generated.ConnectionUpdate
+import io.airbyte.api.client2.model.generated.Geography
 import io.airbyte.api.server.helpers.ConnectionHelper
 import jakarta.validation.constraints.NotBlank
 import java.util.UUID
@@ -31,62 +31,57 @@ object ConnectionUpdateMapper {
    * @return ConnectionCreate Response object to be sent to config api
    */
   fun from(
-    connectionId: @NotBlank UUID?,
+    connectionId: @NotBlank UUID,
     connectionPatchRequest: ConnectionPatchRequest,
     catalogId: UUID?,
     configuredCatalog: AirbyteCatalog?,
   ): ConnectionUpdate {
-    val connectionUpdateOss = ConnectionUpdate()
-    connectionUpdateOss.connectionId(connectionId)
-    connectionUpdateOss.setName(connectionPatchRequest.getName())
-    if (connectionPatchRequest.getNonBreakingSchemaUpdatesBehavior() != null) {
-      connectionUpdateOss.setNonBreakingChangesPreference(
-        ConnectionHelper.convertNonBreakingSchemaUpdatesBehaviorEnum(connectionPatchRequest.getNonBreakingSchemaUpdatesBehavior()),
-      )
-    }
-    if (connectionPatchRequest.getNamespaceDefinition() != null) {
-      connectionUpdateOss.setNamespaceDefinition(
-        ConnectionHelper.convertNamespaceDefinitionEnum(connectionPatchRequest.getNamespaceDefinition()),
-      )
-    }
-    if (connectionPatchRequest.getNamespaceFormat() != null) {
-      connectionUpdateOss.setNamespaceFormat(connectionPatchRequest.getNamespaceFormat())
-    }
-    if (connectionPatchRequest.getPrefix() != null) {
-      connectionUpdateOss.setPrefix(connectionPatchRequest.getPrefix())
-    }
-
-    // set geography
-    if (connectionPatchRequest.getDataResidency() != null) {
-      connectionUpdateOss.setGeography(Geography.fromValue(connectionPatchRequest.getDataResidency().toString()))
-    }
-
-    // set schedule
-    if (connectionPatchRequest.getSchedule() != null) {
-      connectionUpdateOss.setScheduleType(ConnectionScheduleType.fromValue(connectionPatchRequest.getSchedule().getScheduleType().toString()))
-      if (connectionPatchRequest.getSchedule().getScheduleType() !== ScheduleTypeEnum.MANUAL) {
-        // This should only be set if we're not manual
-        val connectionScheduleDataCron = ConnectionScheduleDataCron()
-        connectionScheduleDataCron.setCronExpression(connectionPatchRequest.getSchedule().getCronExpression())
-        connectionScheduleDataCron.setCronTimeZone("UTC")
-        val connectionScheduleData = ConnectionScheduleData()
-        connectionScheduleData.setCron(connectionScheduleDataCron)
-        connectionUpdateOss.setScheduleData(connectionScheduleData)
-      } else {
-        connectionUpdateOss.setScheduleType(ConnectionScheduleType.MANUAL)
-      }
-    }
-
-    // set streams
-    if (catalogId != null) {
-      connectionUpdateOss.setSourceCatalogId(catalogId)
-    }
-    if (configuredCatalog != null) {
-      connectionUpdateOss.setSyncCatalog(configuredCatalog)
-    }
-    if (connectionPatchRequest.getStatus() != null) {
-      connectionUpdateOss.setStatus(ConnectionStatus.fromValue(connectionPatchRequest.getStatus().toString()))
-    }
-    return connectionUpdateOss
+    return ConnectionUpdate(
+      connectionId = connectionId,
+      name = connectionPatchRequest.name,
+      nonBreakingChangesPreference =
+        connectionPatchRequest.nonBreakingSchemaUpdatesBehavior?.let {
+          ConnectionHelper.convertNonBreakingSchemaUpdatesBehaviorEnum(connectionPatchRequest.nonBreakingSchemaUpdatesBehavior)
+        } ?: null,
+      namespaceDefinition =
+        connectionPatchRequest.namespaceDefinition?.let {
+          ConnectionHelper.convertNamespaceDefinitionEnum(connectionPatchRequest.namespaceDefinition)
+        } ?: null,
+      namespaceFormat = connectionPatchRequest.namespaceFormat,
+      prefix = connectionPatchRequest.prefix,
+      geography =
+        connectionPatchRequest.dataResidency?.let {
+          Geography.valueOf(connectionPatchRequest.dataResidency.toString())
+        } ?: null,
+      scheduleType =
+        connectionPatchRequest.schedule?.let { schedule ->
+          if (schedule.scheduleType !== ScheduleTypeEnum.MANUAL) {
+            ConnectionScheduleType.valueOf(schedule.scheduleType.toString())
+          } else {
+            ConnectionScheduleType.MANUAL
+          }
+        } ?: null,
+      scheduleData =
+        connectionPatchRequest.schedule?.let { schedule ->
+          if (schedule.scheduleType !== ScheduleTypeEnum.MANUAL) {
+            ConnectionScheduleData(
+              basicSchedule = null,
+              cron =
+                ConnectionScheduleDataCron(
+                  cronExpression = schedule.cronExpression,
+                  cronTimeZone = "UTC",
+                ),
+            )
+          } else {
+            null
+          }
+        } ?: null,
+      sourceCatalogId = catalogId,
+      syncCatalog = configuredCatalog,
+      status =
+        connectionPatchRequest.status?.let {
+          ConnectionStatus.valueOf(connectionPatchRequest.status.toString())
+        } ?: null,
+    )
   }
 }

commit b514ad29189610abf8a869ca00fb4c5276bb1e35

Fix connection update api request
Potentially broken in https://github.com/airbytehq/airbyte-platform/commit/986253c7c2ae834d2dfc482b5067baeac401d0bd

I was getting:
```
Unexpected error occurred: No enum constant io.airbyte.api.client.model.generated.ConnectionScheduleType.cron
```

It seems that the connection create mapper was working fine so I just
took the code from there and applied to the connection updated mapper.

airbyte-api-server/src/main/kotlin/io/airbyte/api/server/mappers/ConnectionUpdateMapper.kt
@@ -54,13 +54,10 @@ object ConnectionUpdateMapper {
           Geography.valueOf(connectionPatchRequest.dataResidency.toString())
         } ?: null,
       scheduleType =
-        connectionPatchRequest.schedule?.let { schedule ->
-          if (schedule.scheduleType !== ScheduleTypeEnum.MANUAL) {
-            ConnectionScheduleType.valueOf(schedule.scheduleType.toString())
-          } else {
-            ConnectionScheduleType.MANUAL
-          }
-        } ?: null,
+        connectionPatchRequest.schedule?.scheduleType?.let {
+            scheduleType ->
+          ConnectionScheduleType.decode(scheduleType.toString())
+        } ?: ConnectionScheduleType.MANUAL,
       scheduleData =
         connectionPatchRequest.schedule?.let { schedule ->
           if (schedule.scheduleType !== ScheduleTypeEnum.MANUAL) {
@@ -78,10 +75,7 @@ object ConnectionUpdateMapper {
         } ?: null,
       sourceCatalogId = catalogId,
       syncCatalog = configuredCatalog,
-      status =
-        connectionPatchRequest.status?.let {
-          ConnectionStatus.valueOf(connectionPatchRequest.status.toString())
-        } ?: null,
+      status = connectionPatchRequest.status?.let { status -> ConnectionStatus.decode(status.toString()) } ?: ConnectionStatus.ACTIVE,
     )
   }
 }