46b44994880f7e2f33ffa60dc472c79624dc4d6b
Joey Marshment-Howell <josephkmh@users.noreply.github.com>
2024-07-25 20:30:09 +0100

    fix: allow workspace editors to upload custom connectors (#13242)

--- airbyte-webapp/src/core/utils/rbac/intents.ts
+++ airbyte-webapp/src/core/utils/rbac/intents.ts
@@ -4,12 +4,12 @@ export const intentToRbacQuery = {
   // organization
   ListOrganizationMembers: { resourceType: "ORGANIZATION", role: "MEMBER" },
   UpdateConnectorVersions: { resourceType: "ORGANIZATION", role: "ADMIN" }, // this setting is currently instance-wide, but we've decided to scope to organization permissions for now
-  UploadCustomConnector: { resourceType: "ORGANIZATION", role: "ADMIN" },
   UpdateOrganization: { resourceType: "ORGANIZATION", role: "ADMIN" },
   UpdateOrganizationPermissions: { resourceType: "ORGANIZATION", role: "ADMIN" },
   ViewOrganizationSettings: { resourceType: "ORGANIZATION", role: "READER" },
 
   // workspace
+  UploadCustomConnector: { resourceType: "WORKSPACE", role: "EDITOR" },
   DeleteWorkspace: { resourceType: "WORKSPACE", role: "ADMIN" },
   UpdateWorkspace: [
     { resourceType: "WORKSPACE", role: "ADMIN" },
