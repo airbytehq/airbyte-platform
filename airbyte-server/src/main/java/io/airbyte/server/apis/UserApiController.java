bf5cdedcd90af13e07f446a08df7212f4bfcdc7d
Pedro S. Lopez <pedroslopez@me.com>
2024-08-12 18:14:32 -0400

    chore: remove deprecated user create endpoints (#13484)

--- airbyte-server/src/main/java/io/airbyte/server/apis/UserApiController.java
+++ airbyte-server/src/main/java/io/airbyte/server/apis/UserApiController.java
@@ -14,7 +14,6 @@ import io.airbyte.api.generated.UserApi;
 import io.airbyte.api.model.generated.OrganizationIdRequestBody;
 import io.airbyte.api.model.generated.OrganizationUserReadList;
 import io.airbyte.api.model.generated.UserAuthIdRequestBody;
-import io.airbyte.api.model.generated.UserCreate;
 import io.airbyte.api.model.generated.UserEmailRequestBody;
 import io.airbyte.api.model.generated.UserGetOrCreateByAuthIdResponse;
 import io.airbyte.api.model.generated.UserIdRequestBody;
@@ -46,14 +45,6 @@ public class UserApiController implements UserApi {
 
   public UserApiController(final UserHandler userHandler) {
     this.userHandler = userHandler;
-  }
-
-  @Post("/create")
-  @SecuredUser
-  @Secured({ADMIN, SELF})
-  @Override
-  public UserRead createUser(@Body final UserCreate userCreate) {
-    return ApiHelper.execute(() -> userHandler.createUser(userCreate));
   }
 
   @Post("/get")
