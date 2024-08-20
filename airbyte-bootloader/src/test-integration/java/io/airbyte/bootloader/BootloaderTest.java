d56e077debcc5cb0e27d7eb0a0eb6a9626ada434
Ella Rohm-Ensing <ella@airbyte.io>
2024-08-20 16:26:22 -0700

    feat!: add language column to actor definition version (#13591)
    ## What
    <!--
    * Describe what the change is solving.
    * It helps to add screenshots if it affects the frontend.
    -->
    First part of https://github.com/airbytehq/airbyte-internal-issues/issues/9247
    
    Add a nullable `language` column to `actor_definition_version`
    
    ## Can this PR be safely reverted and rolled back?
    <!--
    * If you know that your be safely rolled back, check YES.*
    * If that is not the case (e.g. a database migration), check NO.
    * If unsure, leave it blank.*
    -->
    - [ ] YES 💚
    - [x] NO ❌

--- airbyte-bootloader/src/test-integration/java/io/airbyte/bootloader/BootloaderTest.java
+++ airbyte-bootloader/src/test-integration/java/io/airbyte/bootloader/BootloaderTest.java
@@ -101,7 +101,7 @@ class BootloaderTest {
 
   // ⚠️ This line should change with every new migration to show that you meant to make a new
   // migration to the prod database
-  private static final String CURRENT_CONFIGS_MIGRATION_VERSION = "0.57.4.014";
+  private static final String CURRENT_CONFIGS_MIGRATION_VERSION = "0.57.4.015";
   private static final String CURRENT_JOBS_MIGRATION_VERSION = "0.57.2.005";
 
   @BeforeEach
