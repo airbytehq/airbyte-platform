242fefb2660dccfb16f6d9758b60367569fef8f2
Parker Mossman <parker@airbyte.io>
2024-08-20 02:53:35 -0700

    chore: add Micronaut Data entity/repository/service for Organizations (#13567)

---
+++ airbyte-data/src/main/kotlin/io/airbyte/data/repositories/entities/Organization.kt
@@ -0,0 +1,33 @@
+package io.airbyte.data.repositories.entities
+
+import io.micronaut.data.annotation.DateCreated
+import io.micronaut.data.annotation.DateUpdated
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+import io.micronaut.data.annotation.event.PrePersist
+import java.util.UUID
+
+@MappedEntity("organization")
+open class Organization(
+  @field:Id
+  var id: UUID? = null,
+  var name: String,
+  var userId: UUID? = null,
+  var email: String,
+  var pba: Boolean = false,
+  var orgLevelBilling: Boolean = false,
+  var tombstone: Boolean = false,
+  @DateCreated
+  var createdAt: java.time.OffsetDateTime? = null,
+  @DateUpdated
+  var updatedAt: java.time.OffsetDateTime? = null,
+) {
+  // Use @PrePersist instead of @AutoPopulated so that we can set the id field
+  // if desired prior to insertion.
+  @PrePersist
+  fun prePersist() {
+    if (id == null) {
+      id = UUID.randomUUID()
+    }
+  }
+}
