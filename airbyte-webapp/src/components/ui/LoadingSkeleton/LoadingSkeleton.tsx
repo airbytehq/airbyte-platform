f5930e72430406e2bf19ebfbdadf273e9d5b16b1
Joey Marshment-Howell <josephkmh@users.noreply.github.com>
2024-08-28 17:11:46 +0200

    docs: add stories for LoadingSkeleton and BorderedTile (#13730)

--- airbyte-webapp/src/components/ui/LoadingSkeleton/LoadingSkeleton.tsx
+++ airbyte-webapp/src/components/ui/LoadingSkeleton/LoadingSkeleton.tsx
@@ -3,5 +3,5 @@ import classNames from "classnames";
 import styles from "./LoadingSkeleton.module.scss";
 
 export const LoadingSkeleton = ({ className }: { className?: string }) => {
-  return <div className={classNames(styles.loadingSkeleton, styles["loadingSkeleton--button"], className)} />;
+  return <div className={classNames(styles.loadingSkeleton, styles["loadingSkeleton--buttonHeight"], className)} />;
 };
