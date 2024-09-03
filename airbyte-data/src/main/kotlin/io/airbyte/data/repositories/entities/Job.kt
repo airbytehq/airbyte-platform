805247b37ae75ba60acf65b893bef9e7b6850330
Jon Tan <jon@airbyte.io>
2024-09-03 14:35:13 -0500

    chore: jobs list -> micronaut data (#13422)

--- airbyte-data/src/main/kotlin/io/airbyte/data/repositories/entities/Job.kt
+++ airbyte-data/src/main/kotlin/io/airbyte/data/repositories/entities/Job.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2020-2024 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.data.repositories.entities
 
 import com.fasterxml.jackson.databind.JsonNode
@@ -8,8 +12,24 @@ import io.micronaut.data.annotation.DateCreated
 import io.micronaut.data.annotation.DateUpdated
 import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.MappedEntity
+import io.micronaut.data.annotation.Relation
 import io.micronaut.data.annotation.TypeDef
 import io.micronaut.data.model.DataType
+import java.util.EnumSet
+
+val REPLICATION_TYPES: Set<JobConfigType> = EnumSet.of(JobConfigType.sync, JobConfigType.reset_connection, JobConfigType.refresh)
+val SYNC_REPLICATION_TYPES: Set<JobConfigType> = EnumSet.of(JobConfigType.sync, JobConfigType.refresh)
+
+val TERMINAL_STATUSES =
+  setOf(
+    JobStatus.failed,
+    JobStatus.succeeded,
+    JobStatus.cancelled,
+  )
+
+val NON_CANCELLED_STATUSES = JobStatus.entries.toSet().minus(JobStatus.cancelled)
+
+val NON_TERMINAL_STATUSES = JobStatus.entries.toSet().minus(TERMINAL_STATUSES)
 
 @MappedEntity("jobs")
 open class Job(
@@ -29,3 +49,29 @@ open class Job(
   @DateUpdated
   var updatedAt: java.time.OffsetDateTime? = null,
 )
+
+@MappedEntity("jobs")
+class JobWithAttempts(
+  id: Long? = null,
+  configType: JobConfigType? = null,
+  scope: String? = null,
+  config: JsonNode? = null,
+  status: JobStatus? = null,
+  startedAt: java.time.OffsetDateTime? = null,
+  createdAt: java.time.OffsetDateTime? = null,
+  updatedAt: java.time.OffsetDateTime? = null,
+  @Relation(
+    value = Relation.Kind.ONE_TO_MANY,
+    mappedBy = "job",
+  )
+  var attempts: List<AttemptWithAssociations>? = null,
+) : Job(
+    id = id,
+    configType = configType,
+    scope = scope,
+    config = config,
+    status = status,
+    startedAt = startedAt,
+    createdAt = createdAt,
+    updatedAt = updatedAt,
+  )
