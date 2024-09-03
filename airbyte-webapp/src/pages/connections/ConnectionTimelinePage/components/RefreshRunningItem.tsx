5eae8bd952dd390bd3c29d789386f46e61bcbe1c
Teal Larson <LARSON.TEAL@GMAIL.COM>
2024-09-03 09:26:14 -0400

    fix: add jobId back for sync fail events (#13709)
    Co-authored-by: Chandler Prall <chandler.prall@gmail.com>

--- airbyte-webapp/src/pages/connections/ConnectionTimelinePage/components/RefreshRunningItem.tsx
+++ airbyte-webapp/src/pages/connections/ConnectionTimelinePage/components/RefreshRunningItem.tsx
@@ -4,6 +4,7 @@ import { Box } from "components/ui/Box";
 import { Text } from "components/ui/Text";
 
 import { ResetStreamsDetails } from "area/connection/components/JobHistoryItem/ResetStreamDetails";
+import { useLocalStorage } from "core/utils/useLocalStorage";
 
 import { ConnectionTimelineEventActions } from "../ConnectionTimelineEventActions";
 import { ConnectionTimelineEventIcon } from "../ConnectionTimelineEventIcon";
@@ -17,6 +18,8 @@ interface RefreshRunningItemProps {
 }
 
 export const RefreshRunningItem: React.FC<RefreshRunningItemProps> = ({ jobId, streams, startedAt }) => {
+  const [showExtendedStats] = useLocalStorage("airbyte_extended-attempts-stats", false);
+
   return (
     <ConnectionTimelineEventItem centered>
       <ConnectionTimelineEventIcon icon="rotate" running />
@@ -25,6 +28,11 @@ export const RefreshRunningItem: React.FC<RefreshRunningItemProps> = ({ jobId, s
           <FormattedMessage id="connection.timeline.refresh_running" values={{ count: streams.length }} />
         </Text>
         <Box pt="xs">{streams.length > 0 && <ResetStreamsDetails names={streams} />}</Box>
+        {showExtendedStats && (
+          <Text as="span" color="grey400" size="sm">
+            <FormattedMessage id="jobs.jobId" values={{ id: jobId }} />
+          </Text>
+        )}
       </ConnectionTimelineEventSummary>
       <ConnectionTimelineEventActions createdAt={startedAt} jobId={jobId} />
     </ConnectionTimelineEventItem>
