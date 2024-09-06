84d599fdd13413a6dbdd208cefdcfa65cbbd5ffa
Ben Church <ben@airbyte.io>
2024-09-06 08:34:51 -0700

    feat(ai-assist): animated loading state (#13841)

--- airbyte-webapp/src/components/connectorBuilder/Builder/Assist/AssistWaiting.tsx
+++ airbyte-webapp/src/components/connectorBuilder/Builder/Assist/AssistWaiting.tsx
@@ -3,19 +3,35 @@ import { FormattedMessage } from "react-intl";
 
 import { Button } from "components/ui/Button";
 import { FlexContainer } from "components/ui/Flex";
+import { LoadingSkeleton } from "components/ui/LoadingSkeleton";
 import { Text } from "components/ui/Text";
 
-import aiProcessing from "./assistwaiting.gif";
 import styles from "./AssistWaiting.module.scss";
 
 interface AssistWaitingProps {
   onSkip: () => void;
 }
 
+const FloatingLoadingSkeleton = () => (
+  <div className={styles.floatingAnimation}>
+    <LoadingSkeleton variant="magic" className={styles.loadingSkeleton} />
+  </div>
+);
+
+const AssistLoadingAnimation = () => {
+  return (
+    <FlexContainer direction="column" gap="lg" className={styles.assistLoadingAnimation}>
+      <FloatingLoadingSkeleton />
+      <FloatingLoadingSkeleton />
+      <FloatingLoadingSkeleton />
+    </FlexContainer>
+  );
+};
+
 export const AssistWaiting: React.FC<AssistWaitingProps> = ({ onSkip }) => {
   return (
     <FlexContainer alignItems="center" direction="column" gap="md" className={styles.container}>
-      <img src={aiProcessing} alt="" className={styles.aiProcessingImage} />
+      <AssistLoadingAnimation />
       <Text size="xl" color="darkBlue">
         <FormattedMessage id="connectorBuilder.assist.waiting.message" />
       </Text>
