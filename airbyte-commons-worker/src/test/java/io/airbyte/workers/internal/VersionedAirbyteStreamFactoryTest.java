ed7517f23af990cefa2baa7bad408d2c9a7bd22e
Jonathan Pearlin <jonathan@airbyte.io>
2024-10-07 12:23:09 -0400

    feat: improve malformed log observability (#14251)

--- airbyte-commons-worker/src/test/java/io/airbyte/workers/internal/VersionedAirbyteStreamFactoryTest.java
+++ airbyte-commons-worker/src/test/java/io/airbyte/workers/internal/VersionedAirbyteStreamFactoryTest.java
@@ -4,6 +4,9 @@
 
 package io.airbyte.workers.internal;
 
+import static io.airbyte.workers.internal.VersionedAirbyteStreamFactory.CONNECTION_ID_NOT_PRESENT;
+import static io.airbyte.workers.internal.VersionedAirbyteStreamFactory.MALFORMED_AIRBYTE_RECORD_LOG_MESSAGE;
+import static io.airbyte.workers.internal.VersionedAirbyteStreamFactory.MALFORMED_NON_AIRBYTE_RECORD_LOG_MESSAGE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.atLeastOnce;
@@ -111,7 +114,7 @@ class VersionedAirbyteStreamFactoryTest {
       final Stream<AirbyteMessage> messageStream = stringToMessageStream(invalidRecord);
 
       assertEquals(Collections.emptyList(), messageStream.collect(Collectors.toList()));
-      verify(logger).info(invalidRecord);
+      verify(logger).info(MALFORMED_NON_AIRBYTE_RECORD_LOG_MESSAGE, CONNECTION_ID_NOT_PRESENT, invalidRecord);
     }
 
     @Test
@@ -131,7 +134,7 @@ class VersionedAirbyteStreamFactoryTest {
       final Stream<AirbyteMessage> messageStream = stringToMessageStream(invalidRecord);
 
       assertEquals(Collections.emptyList(), messageStream.collect(Collectors.toList()));
-      verify(logger).info(invalidRecord);
+      verify(logger).info(MALFORMED_NON_AIRBYTE_RECORD_LOG_MESSAGE, CONNECTION_ID_NOT_PRESENT, invalidRecord);
     }
 
     @Test
@@ -141,7 +144,7 @@ class VersionedAirbyteStreamFactoryTest {
       final Stream<AirbyteMessage> messageStream = stringToMessageStream(invalidRecord);
 
       assertEquals(Collections.emptyList(), messageStream.collect(Collectors.toList()));
-      verify(logger).info(invalidRecord);
+      verify(logger).info(MALFORMED_NON_AIRBYTE_RECORD_LOG_MESSAGE, CONNECTION_ID_NOT_PRESENT, invalidRecord);
     }
 
     @Test
@@ -169,7 +172,7 @@ class VersionedAirbyteStreamFactoryTest {
     void testMalformedRecordShouldOnlyDebugLog(final String invalidRecord) {
       stringToMessageStream(invalidRecord).collect(Collectors.toList());
       verifyBlankedRecordRecordWarning();
-      verify(logger).debug(invalidRecord);
+      verify(logger).debug(MALFORMED_AIRBYTE_RECORD_LOG_MESSAGE, CONNECTION_ID_NOT_PRESENT, invalidRecord);
     }
 
     private VersionedAirbyteStreamFactory getFactory() {
@@ -197,7 +200,7 @@ class VersionedAirbyteStreamFactoryTest {
       final String randomLog = "I should not be send on the same channel than the airbyte messages";
       Assertions.assertThat(getFactory().toAirbyteMessage(randomLog))
           .isEmpty();
-      verify(logger).info(randomLog);
+      verify(logger).info(MALFORMED_NON_AIRBYTE_RECORD_LOG_MESSAGE, CONNECTION_ID_NOT_PRESENT, randomLog);
     }
 
     @Test
@@ -205,7 +208,7 @@ class VersionedAirbyteStreamFactoryTest {
       final String messageLine = "It shouldn't be here" + String.format(VALID_MESSAGE_TEMPLATE, "hello");
       getFactory().toAirbyteMessage(messageLine);
       verifyBlankedRecordRecordWarning();
-      verify(logger).debug(messageLine);
+      verify(logger).debug(MALFORMED_AIRBYTE_RECORD_LOG_MESSAGE, CONNECTION_ID_NOT_PRESENT, messageLine);
     }
 
     @Test
@@ -213,7 +216,7 @@ class VersionedAirbyteStreamFactoryTest {
       final String messageLine = "It shouldn't be here" + String.format(VALID_MESSAGE_TEMPLATE, "hello");
       Assertions.assertThat(getFactory().toAirbyteMessage(messageLine)).isEmpty();
       verifyBlankedRecordRecordWarning();
-      verify(logger).debug(messageLine);
+      verify(logger).debug(MALFORMED_AIRBYTE_RECORD_LOG_MESSAGE, CONNECTION_ID_NOT_PRESENT, messageLine);
     }
 
     @Test
--- airbyte-metrics/metrics-lib/src/main/java/io/airbyte/metrics/lib/MetricTags.java
+++ airbyte-metrics/metrics-lib/src/main/java/io/airbyte/metrics/lib/MetricTags.java
@@ -63,6 +63,7 @@ public class MetricTags {
   public static final String UNKNOWN = "unknown";
   public static final String USER_TYPE = "user_type"; // real user, service account, data plane user, etc
   public static final String LOG_CLIENT_TYPE = "log_client_type";
+  public static final String MALFORMED_LOG_LINE_LENGTH = "malformed_log_line_length";
 
   // payload metric tags
   public static final String URI_NULL = "uri_null";
