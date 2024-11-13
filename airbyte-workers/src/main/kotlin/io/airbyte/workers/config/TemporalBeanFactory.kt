67673e9c0dd85f6223f347fac5c926ede155bfe3
Jimmy Ma <gosusnp@users.noreply.github.com>
2024-11-13 10:18:45 -0800

    chore: disable temporal otel tracing plugin (#14611)

--- airbyte-workers/src/main/kotlin/io/airbyte/workers/config/TemporalBeanFactory.kt
+++ airbyte-workers/src/main/kotlin/io/airbyte/workers/config/TemporalBeanFactory.kt
@@ -5,7 +5,6 @@ package io.airbyte.workers.config
 
 import io.micronaut.context.annotation.Factory
 import io.temporal.client.WorkflowClient
-import io.temporal.opentracing.OpenTracingWorkerInterceptor
 import io.temporal.worker.WorkerFactory
 import io.temporal.worker.WorkerFactoryOptions
 import jakarta.inject.Singleton
@@ -19,7 +18,6 @@ class TemporalBeanFactory {
   fun workerFactory(workflowClient: WorkflowClient): WorkerFactory {
     val workerFactoryOptions =
       WorkerFactoryOptions.newBuilder()
-        .setWorkerInterceptors(OpenTracingWorkerInterceptor())
         .build()
     return WorkerFactory.newInstance(workflowClient, workerFactoryOptions)
   }
