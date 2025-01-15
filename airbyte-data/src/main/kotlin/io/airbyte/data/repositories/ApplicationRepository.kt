80577edf79dd8a065f0f2a5a101bc19f8c04e754
Bryce Groff <bgroff@hawaii.edu>
2025-01-15 12:12:03 -0800

    feat: implement the application service backed by the database (#15018)

---
+++ airbyte-data/src/main/kotlin/io/airbyte/data/repositories/ApplicationRepository.kt
@@ -0,0 +1,22 @@
+package io.airbyte.data.repositories
+
+import io.airbyte.data.repositories.entities.Application
+import io.micronaut.data.jdbc.annotation.JdbcRepository
+import io.micronaut.data.model.query.builder.sql.Dialect
+import io.micronaut.data.repository.PageableRepository
+import java.util.UUID
+
+@JdbcRepository(dialect = Dialect.POSTGRES, dataSource = "config")
+interface ApplicationRepository : PageableRepository<Application, UUID> {
+  fun findByUserId(userId: UUID): List<Application>
+
+  fun findByUserIdAndId(
+    userId: UUID,
+    applicationId: UUID,
+  ): Application?
+
+  fun findByClientIdAndClientSecret(
+    clientId: String?,
+    clientSecret: String?,
+  ): Application?
+}
---
+++ airbyte-data/src/main/kotlin/io/airbyte/data/repositories/entities/Application.kt
@@ -0,0 +1,18 @@
+package io.airbyte.data.repositories.entities
+
+import io.micronaut.data.annotation.DateCreated
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+import java.util.UUID
+
+@MappedEntity("application")
+open class Application(
+  @field:Id
+  var id: UUID? = null,
+  var userId: UUID? = null,
+  var name: String,
+  var clientId: String,
+  var clientSecret: String,
+  @DateCreated
+  var createdAt: java.time.OffsetDateTime? = null,
+)
