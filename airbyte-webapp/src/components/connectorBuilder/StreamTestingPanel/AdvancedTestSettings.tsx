36bd18d32af3c3649f8c6a01f081758a9ae4c607
Lake Mossman <lake@airbyte.io>
2025-02-07 11:24:04 -0800

    refactor: refactor advanced test settings to use our modern form components (#15180)

--- airbyte-webapp/src/components/connectorBuilder/StreamTestingPanel/AdvancedTestSettings.tsx
+++ airbyte-webapp/src/components/connectorBuilder/StreamTestingPanel/AdvancedTestSettings.tsx
@@ -1,19 +1,17 @@
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 import { FormattedMessage, useIntl } from "react-intl";
 import * as yup from "yup";
 
-import { ControlLabels } from "components/LabeledControl";
+import { Form, FormControl } from "components/forms";
 import { Button } from "components/ui/Button";
-import { CodeEditor } from "components/ui/CodeEditor";
 import { FlexContainer, FlexItem } from "components/ui/Flex";
-import { Input } from "components/ui/Input";
 import { Modal, ModalBody, ModalFooter } from "components/ui/Modal";
 import { Tooltip } from "components/ui/Tooltip";
 
 import { TestReadContext, useConnectorBuilderTestRead } from "services/connectorBuilder/ConnectorBuilderStateService";
 
 import styles from "./AdvancedTestSettings.module.scss";
+import { BuilderField } from "../Builder/BuilderField";
 import { jsonString } from "../useBuilderValidationSchema";
 
 const MAX_RECORD_LIMIT = 5000;
@@ -26,11 +24,18 @@ const numericCountField = yup
   .required()
   .transform((value) => (isNaN(value) ? 0 : value));
 const testReadLimitsValidation = yup.object({
-  recordLimit: numericCountField.label("Record limit").max(MAX_RECORD_LIMIT),
-  pageLimit: numericCountField.label("Page limit").max(MAX_PAGE_LIMIT),
-  sliceLimit: numericCountField.label("Partition limit").max(MAX_SLICE_LIMIT),
+  recordLimit: numericCountField.label("Record limit").max(MAX_RECORD_LIMIT).min(1),
+  pageLimit: numericCountField.label("Page limit").max(MAX_PAGE_LIMIT).min(1),
+  sliceLimit: numericCountField.label("Partition limit").max(MAX_SLICE_LIMIT).min(1),
   testState: jsonString,
 });
+
+interface AdvancedTestSettingsFormValues {
+  recordLimit: number;
+  pageLimit: number;
+  sliceLimit: number;
+  testState?: string;
+}
 
 interface AdvancedTestSettingsProps {
   className?: string;
@@ -39,6 +44,7 @@ interface AdvancedTestSettingsProps {
 }
 
 export const AdvancedTestSettings: React.FC<AdvancedTestSettingsProps> = ({ className, isOpen, setIsOpen }) => {
+  const { formatMessage } = useIntl();
   const {
     testReadLimits: { recordLimit, setRecordLimit, pageLimit, setPageLimit, sliceLimit, setSliceLimit, defaultLimits },
     testState,
@@ -66,24 +72,47 @@ export const AdvancedTestSettings: React.FC<AdvancedTestSettingsProps> = ({ clas
         <FormattedMessage id="connectorBuilder.testReadSettings.button" />
       </Tooltip>
       {isOpen && (
-        <AdvancedTestSettingsModal
-          defaultLimits={defaultLimits}
-          recordLimit={recordLimit}
-          setRecordLimit={setRecordLimit}
-          pageLimit={pageLimit}
-          setPageLimit={setPageLimit}
-          sliceLimit={sliceLimit}
-          setSliceLimit={setSliceLimit}
-          testState={testState}
-          setTestState={setTestState}
-          setIsOpen={setIsOpen}
-        />
+        <Modal
+          size="md"
+          onCancel={() => setIsOpen(false)}
+          title={formatMessage({ id: "connectorBuilder.testReadSettings.modalTitle" })}
+        >
+          <Form<AdvancedTestSettingsFormValues>
+            schema={testReadLimitsValidation}
+            defaultValues={{
+              recordLimit,
+              pageLimit,
+              sliceLimit,
+              testState,
+            }}
+            onSubmit={async (data: AdvancedTestSettingsFormValues) => {
+              setRecordLimit(data.recordLimit);
+              setSliceLimit(data.sliceLimit);
+              setPageLimit(data.pageLimit);
+              setTestState(data.testState ?? "");
+              setIsOpen(false);
+            }}
+          >
+            <AdvancedTestSettingsForm
+              defaultLimits={defaultLimits}
+              recordLimit={recordLimit}
+              setRecordLimit={setRecordLimit}
+              pageLimit={pageLimit}
+              setPageLimit={setPageLimit}
+              sliceLimit={sliceLimit}
+              setSliceLimit={setSliceLimit}
+              testState={testState}
+              setTestState={setTestState}
+              setIsOpen={setIsOpen}
+            />
+          </Form>
+        </Modal>
       )}
     </>
   );
 };
 
-const AdvancedTestSettingsModal: React.FC<
+const AdvancedTestSettingsForm: React.FC<
   Pick<AdvancedTestSettingsProps, "setIsOpen"> &
     TestReadContext["testReadLimits"] &
     Pick<TestReadContext, "testState" | "setTestState">
@@ -99,114 +128,77 @@ const AdvancedTestSettingsModal: React.FC<
   setTestState,
   setIsOpen,
 }) => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors, isValid },
-    watch,
-    setValue,
-  } = useForm({
-    defaultValues: {
-      recordLimit,
-      pageLimit,
-      sliceLimit,
-      testState,
-    },
-    resolver: yupResolver(testReadLimitsValidation),
-  });
   const { formatMessage } = useIntl();
+  const {
+    formState: { isValid },
+    reset,
+  } = useFormContext<AdvancedTestSettingsFormValues>();
 
   return (
-    <Modal
-      size="md"
-      onCancel={() => setIsOpen(false)}
-      title={formatMessage({ id: "connectorBuilder.testReadSettings.modalTitle" })}
-    >
-      <form
-        onSubmit={handleSubmit((data) => {
-          setRecordLimit(data.recordLimit);
-          setSliceLimit(data.sliceLimit);
-          setPageLimit(data.pageLimit);
-          setTestState(data.testState);
-          setIsOpen(false);
-        })}
-      >
-        <ModalBody className={styles.body}>
-          <ControlLabels
-            label={formatMessage({ id: "connectorBuilder.testReadSettings.recordLimit" })}
-            error={Boolean(errors.recordLimit)}
-            message={errors.recordLimit?.message}
-          >
-            {/* Adding step="50" would make this input's arrow buttons useful, but the browser validation is a bad UX */}
-            <Input {...register("recordLimit")} type="number" />
-          </ControlLabels>
-          <ControlLabels
-            label={formatMessage({ id: "connectorBuilder.testReadSettings.pageLimit" })}
-            error={Boolean(errors.pageLimit)}
-            message={errors.pageLimit?.message}
-          >
-            <Input {...register("pageLimit")} type="number" />
-          </ControlLabels>
-          <ControlLabels
-            label={formatMessage({ id: "connectorBuilder.testReadSettings.sliceLimit" })}
-            error={Boolean(errors.sliceLimit)}
-            message={errors.sliceLimit?.message}
-          >
-            <Input {...register("sliceLimit")} type="number" />
-          </ControlLabels>
-          <ControlLabels
-            label={formatMessage({ id: "connectorBuilder.testReadSettings.testState" })}
-            error={Boolean(errors.testState)}
-            message={errors.testState ? formatMessage({ id: errors.testState.message }) : undefined}
-            className={styles.stateEditorContainer}
-          >
-            <CodeEditor
-              value={watch("testState")}
-              language="json"
-              onChange={(val: string | undefined) => {
-                setValue("testState", val ?? "", {
-                  shouldValidate: true,
-                  shouldDirty: true,
-                  shouldTouch: true,
-                });
-              }}
-            />
-          </ControlLabels>
-        </ModalBody>
-        <ModalFooter>
-          <FlexContainer className={styles.footer}>
-            <FlexItem grow>
-              <Button
-                type="button"
-                variant="danger"
-                onClick={() => {
-                  setRecordLimit(defaultLimits.recordLimit);
-                  setSliceLimit(defaultLimits.sliceLimit);
-                  setPageLimit(defaultLimits.pageLimit);
-                  setTestState("");
-                  reset({ ...defaultLimits });
-                }}
-              >
-                <FormattedMessage id="form.reset" />
-              </Button>
-            </FlexItem>
+    <>
+      <ModalBody className={styles.body}>
+        <FormControl<AdvancedTestSettingsFormValues>
+          fieldType="input"
+          type="number"
+          name="recordLimit"
+          min={1}
+          max={MAX_RECORD_LIMIT}
+          label={formatMessage({ id: "connectorBuilder.testReadSettings.recordLimit" })}
+        />
+        <FormControl<AdvancedTestSettingsFormValues>
+          fieldType="input"
+          type="number"
+          name="pageLimit"
+          min={1}
+          max={MAX_PAGE_LIMIT}
+          label={formatMessage({ id: "connectorBuilder.testReadSettings.pageLimit" })}
+        />
+        <FormControl<AdvancedTestSettingsFormValues>
+          fieldType="input"
+          type="number"
+          name="sliceLimit"
+          min={1}
+          max={MAX_SLICE_LIMIT}
+          label={formatMessage({ id: "connectorBuilder.testReadSettings.sliceLimit" })}
+        />
+        <BuilderField
+          type="jsoneditor"
+          path="testState"
+          label={formatMessage({ id: "connectorBuilder.testReadSettings.testState" })}
+        />
+      </ModalBody>
+      <ModalFooter>
+        <FlexContainer className={styles.footer}>
+          <FlexItem grow>
             <Button
               type="button"
-              variant="secondary"
+              variant="danger"
               onClick={() => {
-                reset({ recordLimit, pageLimit, sliceLimit });
-                setIsOpen(false);
+                setRecordLimit(defaultLimits.recordLimit);
+                setSliceLimit(defaultLimits.sliceLimit);
+                setPageLimit(defaultLimits.pageLimit);
+                setTestState("");
+                reset({ ...defaultLimits });
               }}
             >
-              <FormattedMessage id="form.cancel" />
+              <FormattedMessage id="form.reset" />
             </Button>
-            <Button type="submit" disabled={!isValid}>
-              <FormattedMessage id="form.saveChanges" />
-            </Button>
-          </FlexContainer>
-        </ModalFooter>
-      </form>
-    </Modal>
+          </FlexItem>
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={() => {
+              reset({ recordLimit, pageLimit, sliceLimit, testState });
+              setIsOpen(false);
+            }}
+          >
+            <FormattedMessage id="form.cancel" />
+          </Button>
+          <Button type="submit" disabled={!isValid}>
+            <FormattedMessage id="form.saveChanges" />
+          </Button>
+        </FlexContainer>
+      </ModalFooter>
+    </>
   );
 };
