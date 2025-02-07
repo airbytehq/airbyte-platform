c9cbc20415b5e17674f617acc74f99e018a4543e
Pedro S. Lopez <pedroslopez@me.com>
2025-02-07 10:11:25 -0400

    feat: read enterprise connectors from license (#15197)

--- airbyte-commons-license/src/test/kotlin/io/airbyte/commons/license/AirbyteLicenseReaderTest.kt
+++ airbyte-commons-license/src/test/kotlin/io/airbyte/commons/license/AirbyteLicenseReaderTest.kt
@@ -4,22 +4,43 @@
 
 package io.airbyte.commons.license
 
+import com.fasterxml.jackson.databind.JsonNode
+import io.airbyte.commons.json.Jsons
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.time.ZoneOffset
+import java.util.Base64
+import java.util.UUID
 
 internal class AirbyteLicenseReaderTest {
   @Test
   fun testDeserializeValidLicense() {
-    val licenseKey =
-      (
-        "eyJhbGciOiJIUzI1NiJ9.eyJsaWNlbnNlIjoicHJvIiwibWF4Tm9kZXMiOjUsIm1heEVkaXRvcnMiOjUsImlhd" +
-          "CI6MTcyNTM4ODE3NSwiaXNzIjoiQWlyYnl0ZSIsImF1ZCI6IkFpcmJ5dGUhIiwic3ViIjoibWFsaWtAYWlyYn" +
-          "l0ZS5pbyIsImV4cCI6MTc1NjMyODI5NjczMX0.yoIedwLBWFySl5zZxLWQ4FdtVot7IbGOspzLhpIFhZo"
+    val payload =
+      Jsons.deserialize(
+        """
+      {
+        "license": "pro",
+        "maxNodes": 5,
+        "maxEditors": 5,
+        "enterpriseConnectorIds": ["3f17c355-5717-446b-8857-8ce7c7144531"],
+        "iat": 1725388175,
+        "iss": "Airbyte",
+        "aud": "Airbyte!",
+        "sub": "malik@airbyte.io",
+        "exp": 1756328296731
+      }
+      """,
       )
+    val licenseKey = makeDummyKey(payload)
+
     val license = AirbyteLicenseReader(licenseKey).extractLicense()
     Assertions.assertEquals(5, license.maxEditors)
     Assertions.assertEquals(5, license.maxNodes)
+    Assertions.assertEquals(1, license.enterpriseConnectorIds.size)
+    Assertions.assertEquals(
+      UUID.fromString("3f17c355-5717-446b-8857-8ce7c7144531"),
+      license.enterpriseConnectorIds.first(),
+    )
     Assertions.assertEquals(
       2025,
       license.expirationDate!!
@@ -51,13 +72,27 @@ internal class AirbyteLicenseReaderTest {
 
   @Test
   fun testDeserializeInvalidLicense() {
-    val licenseKey =
-      (
-        "eyJhbGciOiJIUzI1NiJ9.eyJtYXhOb2RlcyI6NSwibWF4RWRpdG9ycyI6NSwiaWF0IjoxNzI1Mzg4MTc1LCJpc3M" +
-          "iOiJBaXJieXRlIiwiYXVkIjoiQWlyYnl0ZSEiLCJzdWIiOiJtYWxpa0BhaXJieXRlLmlvIiwiZXhwIjoxNzU2MzI" +
-          "4Mjk2NzMxfQ.FM_kuEhbA0SGgIZBPDjN9B_hX-a0LBSNvcgeIh4RCyg"
+    val payload =
+      Jsons.deserialize(
+        """
+      {
+        "maxNodes": 5,
+        "maxEditors": 5,
+        "iat": 1725388175,
+        "iss": "Airbyte",
+        "aud": "Airbyte!",
+        "sub": "malik@airbyte.io",
+        "exp": 1756328296731
+      }
+      """,
       )
+    val licenseKey = makeDummyKey(payload)
     val license = AirbyteLicenseReader(licenseKey).extractLicense()
     Assertions.assertSame(AirbyteLicense.LicenseType.INVALID, license.type)
+  }
+
+  private fun makeDummyKey(payload: JsonNode): String {
+    val encodedPayload = Base64.getEncoder().encodeToString(Jsons.serialize(payload).toByteArray())
+    return "HEADER.$encodedPayload.SIGNATURE"
   }
 }
