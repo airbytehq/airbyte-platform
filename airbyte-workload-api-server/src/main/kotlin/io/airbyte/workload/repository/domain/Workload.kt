7e6f579ec0a2276aa31b529fbf0f5c2777e8e5dc
Cole Snodgrass <cole@airbyte.io>
2025-02-25 08:24:59 -0800

    chore: remove more unneccesary depenedencies (#15360)

--- airbyte-workload-api-server/src/main/kotlin/io/airbyte/workload/repository/domain/Workload.kt
+++ airbyte-workload-api-server/src/main/kotlin/io/airbyte/workload/repository/domain/Workload.kt
@@ -17,7 +17,6 @@ import io.micronaut.data.annotation.Relation
 import io.micronaut.data.annotation.TypeDef
 import io.micronaut.data.model.DataType
 import jakarta.inject.Singleton
-import org.jsoup.internal.Normalizer.lowerCase
 import java.time.OffsetDateTime
 import java.util.Optional
 import java.util.UUID
@@ -105,7 +104,7 @@ enum class WorkloadStatus {
   CANCELLED,
   ;
 
-  override fun toString(): String = lowerCase(this.name)
+  override fun toString(): String = name.lowercase()
 }
 
 @Factory
@@ -129,7 +128,7 @@ enum class WorkloadType {
   SPEC,
   ;
 
-  override fun toString(): String = lowerCase(this.name)
+  override fun toString(): String = name.lowercase()
 
   @Factory
   class WorkloadTypeTypeConverters {
