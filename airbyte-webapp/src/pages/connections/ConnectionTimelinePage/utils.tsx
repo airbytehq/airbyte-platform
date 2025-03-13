4f676bd45c8a2a76189cc29be3cc58a0119cc996
Vladimir <dizel852@gmail.com>
2025-03-13 22:06:34 +0200

    feat: add "schema_config_update" option to filter on connection timeline page (#15522)

--- airbyte-webapp/src/pages/connections/ConnectionTimelinePage/utils.tsx
+++ airbyte-webapp/src/pages/connections/ConnectionTimelinePage/utils.tsx
@@ -74,7 +74,7 @@ export const getStatusIcon = (jobStatus: "failed" | "incomplete" | "cancelled" |
 
 export interface TimelineFilterValues {
   status: "success" | "failure" | "incomplete" | "cancelled" | "";
-  eventCategory: "sync" | "clear" | "refresh" | "connection_settings" | "schema_update" | "";
+  eventCategory: "sync" | "clear" | "refresh" | "connection_settings" | "schema_update" | "schema_config_update" | "";
   startDate: string;
   endDate: string;
   eventId: string;
@@ -136,6 +136,7 @@ export const eventTypeByTypeFilterValue: Record<
     ConnectionEventType.CONNECTION_SETTINGS_UPDATE,
   ],
   schema_update: [ConnectionEventType.SCHEMA_UPDATE],
+  schema_config_update: [ConnectionEventType.SCHEMA_CONFIG_UPDATE],
 };
 
 export const getStatusByEventType = (eventType: ConnectionEventType) => {
@@ -232,6 +233,7 @@ export const eventTypeFilterOptions = (filterValues: TimelineFilterValues) => {
       ? [
           generateEventTypeFilterOption("connection_settings", "connection.timeline.filters.connection_settings"),
           generateEventTypeFilterOption("schema_update", "connection.timeline.filters.schema_update"),
+          generateEventTypeFilterOption("schema_config_update", "connection.timeline.filters.schema_config_update"),
         ]
       : []),
   ];
