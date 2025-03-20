a457ab04f74eba638db39a8f9696c701b8b6dae6
Teal Larson <teal@airbyte.io>
2025-03-20 10:19:46 -0400

    chore: ⚠️ contains migration ⚠️ add config_template and partial_user_config tables to configdb (#15596)

--- airbyte-bootloader/src/test-integration/kotlin/io/airbyte/bootloader/BootloaderTest.kt
+++ airbyte-bootloader/src/test-integration/kotlin/io/airbyte/bootloader/BootloaderTest.kt
@@ -46,7 +46,7 @@ import io.airbyte.db.factory.FlywayFactory
 import io.airbyte.db.instance.DatabaseConstants
 import io.airbyte.db.instance.configs.ConfigsDatabaseMigrator
 import io.airbyte.db.instance.configs.ConfigsDatabaseTestProvider
-import io.airbyte.db.instance.configs.migrations.V1_1_1_017__AddOrganizationIdToActorOauthParameter
+import io.airbyte.db.instance.configs.migrations.V1_1_1_018__AddConfigTemplateAndPartialUserConfigTables
 import io.airbyte.db.instance.jobs.JobsDatabaseMigrator
 import io.airbyte.db.instance.jobs.JobsDatabaseTestProvider
 import io.airbyte.db.instance.jobs.migrations.V1_1_0_001__AddIsScheduledToJobTable
@@ -816,8 +816,7 @@ internal class BootloaderTest {
 
     // ⚠️ This line should change with every new migration to show that you meant to make a new
     // migration to the prod database
-
-    private val CURRENT_CONFIGS_MIGRATION = V1_1_1_017__AddOrganizationIdToActorOauthParameter::class.java
+    private val CURRENT_CONFIGS_MIGRATION = V1_1_1_018__AddConfigTemplateAndPartialUserConfigTables::class.java
     private val CURRENT_JOBS_MIGRATION = V1_1_0_001__AddIsScheduledToJobTable::class.java
 
     private fun getMigrationVersion(cls: Class<*>): String =
