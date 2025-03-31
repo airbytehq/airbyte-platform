b1d90dc88545514b9a32d1b80b1f92e6bab62896
Alexandre Girard <alexandre@airbyte.io>
2025-03-31 14:12:01 -0700

    feat: create oauth_state table (#15755)

--- airbyte-bootloader/src/test-integration/kotlin/io/airbyte/bootloader/BootloaderTest.kt
+++ airbyte-bootloader/src/test-integration/kotlin/io/airbyte/bootloader/BootloaderTest.kt
@@ -46,7 +46,7 @@ import io.airbyte.db.factory.FlywayFactory
 import io.airbyte.db.instance.DatabaseConstants
 import io.airbyte.db.instance.configs.ConfigsDatabaseMigrator
 import io.airbyte.db.instance.configs.ConfigsDatabaseTestProvider
-import io.airbyte.db.instance.configs.migrations.V1_1_1_024__DropAndAddFkRestraintsForPartialUserConfig
+import io.airbyte.db.instance.configs.migrations.V1_1_1_025__CreateOAuthStateTable
 import io.airbyte.db.instance.jobs.JobsDatabaseMigrator
 import io.airbyte.db.instance.jobs.JobsDatabaseTestProvider
 import io.airbyte.db.instance.jobs.migrations.V1_1_0_001__AddIsScheduledToJobTable
@@ -774,7 +774,7 @@ internal class BootloaderTest {
 
     // ⚠️ This line should change with every new migration to show that you meant to make a new
     // migration to the prod database
-    private val CURRENT_CONFIGS_MIGRATION = V1_1_1_024__DropAndAddFkRestraintsForPartialUserConfig::class.java
+    private val CURRENT_CONFIGS_MIGRATION = V1_1_1_025__CreateOAuthStateTable::class.java
     private val CURRENT_JOBS_MIGRATION = V1_1_0_001__AddIsScheduledToJobTable::class.java
 
     private fun getMigrationVersion(cls: Class<*>): String =
