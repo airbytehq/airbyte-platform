ed382147e68f39b8729fc59d58eb56f2406b2671
Jon Tan <jon@airbyte.io>
2025-04-11 10:15:09 -0700

    fix: update workload token validator (#15897)

--- airbyte-workload-api-server/src/main/kotlin/io/airbyte/workload/auth/WorkloadTokenValidator.kt
+++ airbyte-workload-api-server/src/main/kotlin/io/airbyte/workload/auth/WorkloadTokenValidator.kt
@@ -6,20 +6,17 @@ package io.airbyte.workload.auth
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.micronaut.context.annotation.Value
+import io.micronaut.core.order.Ordered
 import io.micronaut.http.HttpRequest
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.token.validator.TokenValidator
 import jakarta.inject.Singleton
 import org.reactivestreams.Publisher
-import reactor.core.publisher.Flux
-import reactor.core.publisher.FluxSink
+import reactor.core.publisher.Mono
 import java.util.Base64
 
 private val LOGGER = KotlinLogging.logger {}
 
-/**
- * Validates that the token extracted from the HTTP request has access to the workload API.
- */
 @Singleton
 class WorkloadTokenValidator(
   @Value("\${airbyte.workload-api.bearer-token.secret}") val bearerSecret: String,
@@ -28,21 +25,22 @@ class WorkloadTokenValidator(
     token: String,
     request: HttpRequest<*>?,
   ): Publisher<Authentication> =
-    Flux.create<Authentication> { emitter: FluxSink<Authentication?> ->
-      try {
-        if (authenticateRequest(token)) {
-          LOGGER.debug { "Request authorized." }
-          emitter.next(Authentication.build(WORKLOAD_API_USER))
-          emitter.complete()
-        } else {
-          LOGGER.debug { "Request denied." }
-          emitter.complete()
-        }
-      } catch (exception: Exception) {
-        LOGGER.debug(exception) { "Error validating token" }
-        emitter.complete()
+    try {
+      if (authenticateRequest(token)) {
+        LOGGER.debug { "Request authorized." }
+        Mono.just(Authentication.build(WORKLOAD_API_USER))
+      } else {
+        LOGGER.debug { "Request denied." }
+        Mono.empty()
       }
+    } catch (exception: Exception) {
+      LOGGER.debug(exception) { "Error validating token" }
+      Mono.empty()
     }
+
+  // This validator should run last.
+  // This hopefully allows the default NimbusReactiveJsonWebTokenValidator to run and use micronaut security to validate JWTs.
+  override fun getOrder(): Int = Ordered.LOWEST_PRECEDENCE
 
   private fun authenticateRequest(token: String): Boolean =
     Base64
