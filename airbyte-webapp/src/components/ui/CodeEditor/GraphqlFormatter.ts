839e67c7d6e0fddcb2e338b39b7513dbdcde911c
Christo Grabowski <108154848+ChristoGrab@users.noreply.github.com>
2025-04-21 11:49:22 -0700

    fix: handle graphql query formatting in Cloud (#16007)

--- airbyte-webapp/src/components/ui/CodeEditor/GraphqlFormatter.ts
+++ airbyte-webapp/src/components/ui/CodeEditor/GraphqlFormatter.ts
@@ -2,10 +2,15 @@ import { parse, print } from "graphql";
 
 export const formatGraphqlQuery = (query: string): string => {
   // Returns a formatted Graphql query string.
-  // If the "query" keyword was already present, we re-inject it at the beginning of the string
-  // for consistency and clarity. Otherwise, we return the formatted query as is.
-  if (query.startsWith("query")) {
+  // In most cases, we simply return the formatted query as is.
+  // On local OSS instance, the "query" keyword is removed by the formatter.
+  // When this happens, we re-inject it to not alter the user's input.
+
+  const formattedQuery = print(parse(query));
+
+  if (query.startsWith("query") && !formattedQuery.startsWith("query")) {
     return `query ${print(parse(query))}`;
   }
-  return `${print(parse(query))}`;
+
+  return formattedQuery;
 };
