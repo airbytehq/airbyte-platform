f4150fa11c90c6239fb4d902b2f66f59bb65799f
Parker Mossman <parker@airbyte.io>
2025-04-21 12:45:01 -0700

    chore: remove CreditProcessingCron and TemporaryBillingValidationWorkflow (#16006)

--- airbyte-featureflag/src/main/kotlin/FlagDefinitions.kt
+++ airbyte-featureflag/src/main/kotlin/FlagDefinitions.kt
@@ -173,8 +173,6 @@ object AllowSpotInstances : Temporary<Boolean>(key = "platform.allow-spot-instan
 
 object HydrateLimits : Temporary<Boolean>(key = "platform.hydrate.limits", default = false)
 
-object BillingPushToOrbWithTemporalNotCron : Temporary<Boolean>(key = "billing.push-to-orb-with-temporal-not-cron", default = false)
-
 object LicenseAllowEnterpriseConnector : Permanent<Boolean>(key = "license.allow-enterprise-connector", default = false)
 
 object AllowConfigTemplateEndpoints : Permanent<Boolean>(key = "platform.allow-config-template-endpoints", default = false)
