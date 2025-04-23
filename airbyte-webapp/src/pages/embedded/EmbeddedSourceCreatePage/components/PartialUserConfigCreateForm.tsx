7c9a722de4ac787d0ce7905c2b02e093ad62fabc
Teal Larson <teal@airbyte.io>
2025-04-23 16:54:01 -0400

    feat: widget styling updates (#16048)

--- airbyte-webapp/src/pages/embedded/EmbeddedSourceCreatePage/components/PartialUserConfigCreateForm.tsx
+++ airbyte-webapp/src/pages/embedded/EmbeddedSourceCreatePage/components/PartialUserConfigCreateForm.tsx
@@ -1,23 +1,20 @@
-import { useSearchParams } from "react-router-dom";
-
 import { useCreatePartialUserConfig, useGetConfigTemplate } from "core/api";
 import { SourceDefinitionSpecificationDraft } from "core/domain/connector";
 import { ConnectorFormValues } from "views/Connector/ConnectorForm";
 
 import { PartialUserConfigForm } from "./PartialUserConfigForm";
+import { useEmbeddedSourceParams } from "../hooks/useEmbeddedSourceParams";
 
 export const PartialUserConfigCreateForm: React.FC = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const selectedTemplateId = searchParams.get("selectedTemplateId") ?? "";
-  const workspaceId = searchParams.get("workspaceId") ?? "";
-  const { mutate: createPartialUserConfig } = useCreatePartialUserConfig();
-  const configTemplate = useGetConfigTemplate(selectedTemplateId, workspaceId);
+  const { workspaceId, selectedTemplateId } = useEmbeddedSourceParams();
+  const { mutate: createPartialUserConfig, isSuccess } = useCreatePartialUserConfig();
+  const configTemplate = useGetConfigTemplate(selectedTemplateId ?? "", workspaceId);
 
   const sourceDefinitionSpecification: SourceDefinitionSpecificationDraft = configTemplate.configTemplateSpec;
   const onSubmit = (values: ConnectorFormValues) => {
     createPartialUserConfig({
       workspaceId,
-      configTemplateId: selectedTemplateId,
+      configTemplateId: selectedTemplateId ?? "",
       connectionConfiguration: values.connectionConfiguration,
     });
   };
@@ -25,15 +22,11 @@ export const PartialUserConfigCreateForm: React.FC = () => {
   return (
     <PartialUserConfigForm
       isEditMode={false}
-      title={configTemplate.name}
-      onBack={() => {
-        setSearchParams((params) => {
-          params.delete("selectedTemplateId");
-          return params;
-        });
-      }}
+      connectorName={configTemplate.name}
+      icon={configTemplate.icon}
       onSubmit={onSubmit}
       sourceDefinitionSpecification={sourceDefinitionSpecification}
+      showSuccessView={isSuccess}
     />
   );
 };
--- airbyte-webapp/src/pages/embedded/EmbeddedSourceCreatePage/components/PartialUserConfigEditForm.tsx
+++ airbyte-webapp/src/pages/embedded/EmbeddedSourceCreatePage/components/PartialUserConfigEditForm.tsx
@@ -1,18 +1,16 @@
-import { useSearchParams } from "react-router-dom";
-
 import { useGetPartialUserConfig, useUpdatePartialUserConfig } from "core/api";
 import { SourceDefinitionSpecificationDraft } from "core/domain/connector";
 import { ConnectorFormValues } from "views/Connector/ConnectorForm";
 
 import { PartialUserConfigForm } from "./PartialUserConfigForm";
-import { SELECTED_PARTIAL_CONFIG_ID_PARAM, WORKSPACE_ID_PARAM } from "../hooks/useEmbeddedSourceParams";
+import { useEmbeddedSourceParams } from "../hooks/useEmbeddedSourceParams";
 
 export const PartialUserConfigEditForm: React.FC = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const selectedPartialConfigId = searchParams.get(SELECTED_PARTIAL_CONFIG_ID_PARAM);
-  const { mutate: updatePartialUserConfig } = useUpdatePartialUserConfig();
+  const { selectedPartialConfigId, workspaceId } = useEmbeddedSourceParams();
+  const { mutate: updatePartialUserConfig, isSuccess } = useUpdatePartialUserConfig();
+
   const partialUserConfig = useGetPartialUserConfig(selectedPartialConfigId ?? "");
-  const workspaceId = searchParams.get(WORKSPACE_ID_PARAM) ?? "";
+
   const sourceDefinitionSpecification: SourceDefinitionSpecificationDraft =
     partialUserConfig.configTemplate.configTemplateSpec;
 
@@ -32,16 +30,12 @@ export const PartialUserConfigEditForm: React.FC = () => {
   return (
     <PartialUserConfigForm
       isEditMode
-      title={partialUserConfig.configTemplate.name}
-      onBack={() => {
-        setSearchParams((params) => {
-          params.delete("selectedPartialConfigId");
-          return params;
-        });
-      }}
+      connectorName={partialUserConfig.configTemplate.name}
+      icon={partialUserConfig.configTemplate.icon}
       onSubmit={onSubmit}
       initialValues={initialValues}
       sourceDefinitionSpecification={sourceDefinitionSpecification}
+      showSuccessView={isSuccess}
     />
   );
 };
