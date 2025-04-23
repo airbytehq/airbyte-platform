d284a342afb55acbb435836970321011580537f0
Lukas Andersen <lukas.andersen@airbyte.io>
2025-04-23 12:01:03 -0700

    feat: add initial org picker (#15997)

--- airbyte-webapp/src/views/layout/SideBar/SideBar.tsx
+++ airbyte-webapp/src/views/layout/SideBar/SideBar.tsx
@@ -9,8 +9,10 @@ import { Icon } from "components/ui/Icon";
 import { ThemeToggle } from "components/ui/ThemeToggle";
 import { WorkspacesPicker } from "components/workspace/WorkspacesPicker";
 
+import { AirbyteOrgPicker } from "area/sidebar/components/AirbyteOrgPicker";
 import { useAuthService } from "core/services/auth";
 import { FeatureItem, IfFeatureEnabled } from "core/services/features";
+import { useExperiment } from "hooks/services/Experiment";
 import { ConnectorBuilderRoutePaths } from "pages/connectorBuilder/ConnectorBuilderRoutes";
 import { RoutePaths } from "pages/routePaths";
 
@@ -43,80 +45,88 @@ export const SideBar: React.FC<PropsWithChildren<SideBarProps>> = ({ bottomSlot,
     return user?.name?.trim() || user?.email?.trim();
   }, [authType, user?.name, user?.email, formatMessage]);
 
+  const isOrgPickerEnabled = useExperiment("sidebar.showOrgPicker");
+
   return (
     <nav className={classNames(styles.sidebar, { [styles.hidden]: isHidden })}>
-      <AirbyteHomeLink />
-      <IfFeatureEnabled feature={FeatureItem.ShowAdminWarningInWorkspace}>
-        <AdminWorkspaceWarning />
-      </IfFeatureEnabled>
-      <IfFeatureEnabled feature={FeatureItem.MultiWorkspaceUI}>
-        <WorkspacesPicker />
-      </IfFeatureEnabled>
-      <FlexContainer className={styles.sidebar__menuItems} direction="column" justifyContent="space-between">
-        <MenuContent data-testid="navMainItems">
-          <NavItem
-            label={<FormattedMessage id="sidebar.connections" />}
-            icon="connection"
-            to={RoutePaths.Connections}
-            testId="connectionsLink"
-          />
-          <NavItem
-            label={<FormattedMessage id="sidebar.sources" />}
-            icon="source"
-            to={RoutePaths.Source}
-            testId="sourcesLink"
-          />
-          <NavItem
-            label={<FormattedMessage id="sidebar.destinations" />}
-            icon="destination"
-            testId="destinationsLink"
-            to={RoutePaths.Destination}
-          />
-          <NavItem
-            label={<FormattedMessage id="sidebar.builder" />}
-            icon="wrench"
-            testId="builderLink"
-            to={RoutePaths.ConnectorBuilder}
-          />
-          <NavItem
-            label={<FormattedMessage id="sidebar.settings" />}
-            icon="gear"
-            to={RoutePaths.Settings}
-            withNotification={settingHighlight}
-          />
-        </MenuContent>
-        <MenuContent>
-          {bottomSlot}
-          <ThemeToggle />
-          {logout && user && (
-            <NavDropdown
-              buttonTestId="sidebar.userDropdown"
-              onChange={({ value }) => {
-                value === "logout" && logout();
-              }}
-              options={[
-                {
-                  as: "a",
-                  href: RoutePaths.Settings,
-                  displayName: formatMessage({ id: "sidebar.userSettings" }),
-                  internal: true,
-                  icon: <Icon type="gear" />,
-                },
-                {
-                  as: "button",
-                  displayName: formatMessage({ id: "sidebar.logout" }),
-                  icon: <Icon type="signout" />,
-                  value: "logout",
-                  className: styles.sidebar__logoutButton,
-                  "data-testid": "sidebar.signout",
-                },
-              ]}
-              icon="user"
-              label={username}
-            />
-          )}
-        </MenuContent>
-      </FlexContainer>
+      {isOrgPickerEnabled ? (
+        <AirbyteOrgPicker />
+      ) : (
+        <>
+          <AirbyteHomeLink />
+          <IfFeatureEnabled feature={FeatureItem.ShowAdminWarningInWorkspace}>
+            <AdminWorkspaceWarning />
+          </IfFeatureEnabled>
+          <IfFeatureEnabled feature={FeatureItem.MultiWorkspaceUI}>
+            <WorkspacesPicker />
+          </IfFeatureEnabled>
+          <FlexContainer className={styles.sidebar__menuItems} direction="column" justifyContent="space-between">
+            <MenuContent data-testid="navMainItems">
+              <NavItem
+                label={<FormattedMessage id="sidebar.connections" />}
+                icon="connection"
+                to={RoutePaths.Connections}
+                testId="connectionsLink"
+              />
+              <NavItem
+                label={<FormattedMessage id="sidebar.sources" />}
+                icon="source"
+                to={RoutePaths.Source}
+                testId="sourcesLink"
+              />
+              <NavItem
+                label={<FormattedMessage id="sidebar.destinations" />}
+                icon="destination"
+                testId="destinationsLink"
+                to={RoutePaths.Destination}
+              />
+              <NavItem
+                label={<FormattedMessage id="sidebar.builder" />}
+                icon="wrench"
+                testId="builderLink"
+                to={RoutePaths.ConnectorBuilder}
+              />
+              <NavItem
+                label={<FormattedMessage id="sidebar.settings" />}
+                icon="gear"
+                to={RoutePaths.Settings}
+                withNotification={settingHighlight}
+              />
+            </MenuContent>
+            <MenuContent>
+              {bottomSlot}
+              <ThemeToggle />
+              {logout && user && (
+                <NavDropdown
+                  buttonTestId="sidebar.userDropdown"
+                  onChange={({ value }) => {
+                    value === "logout" && logout();
+                  }}
+                  options={[
+                    {
+                      as: "a",
+                      href: RoutePaths.Settings,
+                      displayName: formatMessage({ id: "sidebar.userSettings" }),
+                      internal: true,
+                      icon: <Icon type="gear" />,
+                    },
+                    {
+                      as: "button",
+                      displayName: formatMessage({ id: "sidebar.logout" }),
+                      icon: <Icon type="signout" />,
+                      value: "logout",
+                      className: styles.sidebar__logoutButton,
+                      "data-testid": "sidebar.signout",
+                    },
+                  ]}
+                  icon="user"
+                  label={username}
+                />
+              )}
+            </MenuContent>
+          </FlexContainer>
+        </>
+      )}
     </nav>
   );
 };
