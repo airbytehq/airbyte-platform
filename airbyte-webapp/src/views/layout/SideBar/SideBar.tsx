e4e47c76ea367db9ce37685a171424a65a1db31a
Lukas Andersen <lukas.andersen@airbyte.io>
2025-04-24 09:36:15 -0700

    feat: add nav to sidebar (#16051)

--- airbyte-webapp/src/views/layout/SideBar/SideBar.tsx
+++ airbyte-webapp/src/views/layout/SideBar/SideBar.tsx
@@ -4,6 +4,7 @@ import { FormattedMessage, useIntl } from "react-intl";
 import { matchPath, useLocation } from "react-router-dom";
 
 import { AdminWorkspaceWarning } from "components/ui/AdminWorkspaceWarning";
+import { Box } from "components/ui/Box";
 import { FlexContainer } from "components/ui/Flex";
 import { Icon } from "components/ui/Icon";
 import { ThemeToggle } from "components/ui/ThemeToggle";
@@ -50,7 +51,78 @@ export const SideBar: React.FC<PropsWithChildren<SideBarProps>> = ({ bottomSlot,
   return (
     <nav className={classNames(styles.sidebar, { [styles.hidden]: isHidden })}>
       {isOrgPickerEnabled ? (
-        <AirbyteOrgPicker />
+        <>
+          <AirbyteOrgPicker />
+          <IfFeatureEnabled feature={FeatureItem.ShowAdminWarningInWorkspace}>
+            <AdminWorkspaceWarning />
+          </IfFeatureEnabled>
+          <FlexContainer className={styles.sidebar__menuItems} direction="column" justifyContent="flex-start">
+            <MenuContent data-testid="navMainItems">
+              <NavItem
+                label={<FormattedMessage id="sidebar.connections" />}
+                icon="connection"
+                to={RoutePaths.Connections}
+                testId="connectionsLink"
+              />
+              <NavItem
+                label={<FormattedMessage id="sidebar.sources" />}
+                icon="source"
+                to={RoutePaths.Source}
+                testId="sourcesLink"
+              />
+              <NavItem
+                label={<FormattedMessage id="sidebar.destinations" />}
+                icon="destination"
+                testId="destinationsLink"
+                to={RoutePaths.Destination}
+              />
+              <NavItem
+                label={<FormattedMessage id="sidebar.builder" />}
+                icon="wrench"
+                testId="builderLink"
+                to={RoutePaths.ConnectorBuilder}
+              />
+              <NavItem
+                label={<FormattedMessage id="sidebar.settings" />}
+                icon="gear"
+                to={RoutePaths.Settings}
+                withNotification={settingHighlight}
+              />
+            </MenuContent>
+            <Box className={styles.sidebar__menuContentSeparator} />
+            <MenuContent>
+              {bottomSlot}
+              <ThemeToggle />
+              {logout && user && (
+                <NavDropdown
+                  buttonTestId="sidebar.userDropdown"
+                  onChange={({ value }) => {
+                    value === "logout" && logout();
+                  }}
+                  options={[
+                    {
+                      as: "a",
+                      href: RoutePaths.Settings,
+                      displayName: formatMessage({ id: "sidebar.userSettings" }),
+                      internal: true,
+                      icon: <Icon type="gear" />,
+                    },
+                    {
+                      as: "button",
+                      displayName: formatMessage({ id: "sidebar.logout" }),
+                      icon: <Icon type="signout" />,
+                      value: "logout",
+                      className: styles.sidebar__logoutButton,
+                      "data-testid": "sidebar.signout",
+                    },
+                  ]}
+                  icon="user"
+                  label={username}
+                />
+              )}
+            </MenuContent>
+          </FlexContainer>
+        </>
       ) : (
         <>
           <AirbyteHomeLink />
