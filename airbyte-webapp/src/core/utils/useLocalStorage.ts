b57b4e944fcbe7f0af53231e9fc7d3174e787b99
Joey Marshment-Howell <7550957+joeykmh@users.noreply.github.com>
2025-04-28 20:48:08 +0200

    refactor: enterprise license check (#16042)

--- airbyte-webapp/src/core/utils/useLocalStorage.ts
+++ airbyte-webapp/src/core/utils/useLocalStorage.ts
@@ -26,6 +26,7 @@ interface AirbyteLocalStorage {
   "airbyte_ai-assist-projects": Record<string, AssistLocalStorageProject>;
   "airbyte_last-sso-company-identifier": string;
   "airbyte_connector-builder-modes": Record<string, BuilderState["mode"]>;
+  "airbyte_license-check-dismissed-at": string | null;
 }
 
 /*
