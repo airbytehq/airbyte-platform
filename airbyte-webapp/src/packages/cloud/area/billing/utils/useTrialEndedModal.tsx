7c55d87113daca623f5ea699d87bd3a87543075c
Lake Mossman <lake@airbyte.io>
2025-08-21 15:09:45 -0700

    fix: hide trial ended modal behind experiment flag (#17505)

--- airbyte-webapp/src/packages/cloud/area/billing/utils/useTrialEndedModal.tsx
+++ airbyte-webapp/src/packages/cloud/area/billing/utils/useTrialEndedModal.tsx
@@ -1,15 +1,17 @@
 import { TrialEndedModal } from "components/TrialEndedModal/TrialEndedModal";
 
 import { useOrganizationSubscriptionStatus } from "core/utils/useOrganizationSubscriptionStatus";
+import { useExperiment } from "hooks/services/Experiment";
 import { useModalService } from "hooks/services/Modal";
 
 export const useTrialEndedModal = (): void => {
   const { openModal } = useModalService();
+  const showTeamsFeaturesWarnModal = useExperiment("entitlements.showTeamsFeaturesWarnModal");
 
   useOrganizationSubscriptionStatus({
     refetchWithInterval: true,
     onSuccessGetTrialStatusClb: (isTrialEndedAndLockedOrDisabled: boolean) => {
-      if (isTrialEndedAndLockedOrDisabled) {
+      if (showTeamsFeaturesWarnModal && isTrialEndedAndLockedOrDisabled) {
         openModal({
           content: TrialEndedModal,
         });
