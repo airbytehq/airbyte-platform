cf6c40814ac94f14c3d07f8c33d632af91fa4b79
Pedro S. Lopez <pedroslopez@me.com>
2025-08-28 20:46:30 -0400

    chore: manifest-server auth (#17577)

--- airbyte-api/manifest-server-api/src/main/kotlin/io/airbyte/manifestserver/api/client/config/ManifestServerApiClientFactory.kt
+++ airbyte-api/manifest-server-api/src/main/kotlin/io/airbyte/manifestserver/api/client/config/ManifestServerApiClientFactory.kt
@@ -5,6 +5,7 @@
 package io.airbyte.manifestserver.api.client.config
 
 import dev.failsafe.RetryPolicy
+import io.airbyte.api.client.auth.InternalClientTokenInterceptor
 import io.airbyte.manifestserver.api.client.ManifestServerApiClient
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Property
@@ -20,12 +21,15 @@ class ManifestServerApiClientFactory {
   @Singleton
   @Requires(property = "airbyte.manifest-server-api.base-path")
   fun manifestServerApiClient(
+    @Property(name = "micronaut.application.name") applicationName: String,
     @Property(name = "airbyte.manifest-server-api.base-path") basePath: String,
     @Property(name = "airbyte.manifest-server-api.connect-timeout-seconds") connectTimeoutSeconds: Long,
     @Property(name = "airbyte.manifest-server-api.read-timeout-seconds") readTimeoutSeconds: Long,
+    @Property(name = "airbyte.manifest-server-api.signature-secret") signatureSecret: String,
   ): ManifestServerApiClient {
     val builder: OkHttpClient.Builder =
       OkHttpClient.Builder().apply {
+        addInterceptor(InternalClientTokenInterceptor(applicationName, signatureSecret))
         readTimeout(readTimeoutSeconds.seconds.toJavaDuration())
         connectTimeout(connectTimeoutSeconds.seconds.toJavaDuration())
       }
