fd26406820b2fc57d8428d56ee9a12bde5723e3c
Joey Marshment-Howell <7550957+joeykmh@users.noreply.github.com>
2025-09-02 13:51:25 +0200

    feat: add copy workspace id button in settings (#17599)

--- airbyte-webapp/src/packages/cloud/views/workspaces/WorkspaceSettingsView/WorkspaceSettingsView.tsx
+++ airbyte-webapp/src/packages/cloud/views/workspaces/WorkspaceSettingsView/WorkspaceSettingsView.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 
-import { FlexContainer } from "components/ui/Flex";
+import { CopyButton } from "components/ui/CopyButton";
+import { FlexContainer, FlexItem } from "components/ui/Flex";
 import { Heading } from "components/ui/Heading";
 import { Separator } from "components/ui/Separator";
 
+import { useCurrentWorkspaceId } from "area/workspace/utils";
 import { useTrackPage, PageTrackingCodes } from "core/services/analytics";
 import { Intent, useGeneratedIntent } from "core/utils/rbac";
 import { DeleteWorkspace } from "pages/SettingsPage/components/DeleteWorkspace";
@@ -13,14 +15,28 @@ import { TagsTable } from "pages/SettingsPage/Workspace/components/TagsTable";
 import { UpdateWorkspaceSettingsForm } from "./components/UpdateWorkspaceSettingsForm";
 
 export const WorkspaceSettingsView: React.FC = () => {
+  const { formatMessage } = useIntl();
+  const workspaceId = useCurrentWorkspaceId();
   useTrackPage(PageTrackingCodes.SETTINGS_WORKSPACE);
   const canDeleteWorkspace = useGeneratedIntent(Intent.DeleteWorkspace);
 
   return (
     <FlexContainer direction="column" gap="xl">
-      <Heading as="h1" size="md">
-        <FormattedMessage id="settings.workspace.general.title" />
-      </Heading>
+      <FlexContainer alignItems="center" wrap="wrap">
+        <FlexItem grow>
+          <Heading as="h1" size="md">
+            <FormattedMessage id="settings.workspace.general.title" />
+          </Heading>
+        </FlexItem>
+        <CopyButton
+          content={workspaceId}
+          variant="clear"
+          iconPosition="right"
+          title={formatMessage({ id: "settings.workspaceId.copy" })}
+        >
+          <FormattedMessage id="settings.workspaceId" values={{ id: workspaceId }} />
+        </CopyButton>
+      </FlexContainer>
       <UpdateWorkspaceSettingsForm />
       <Separator />
       <TagsTable />
