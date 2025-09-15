298e9c795c66eeaa2cc424e1d6034478f2eb5042
Cole Snodgrass <cole@airbyte.io>
2025-09-15 11:13:29 -0700

    chore: remove additional swagger annotations (#17737)

--- airbyte-workload-api-server/src/main/kotlin/io/airbyte/workload/server/Application.kt
+++ airbyte-workload-api-server/src/main/kotlin/io/airbyte/workload/server/Application.kt
@@ -5,19 +5,7 @@
 package io.airbyte.workload.server
 
 import io.micronaut.runtime.Micronaut.build
-import io.swagger.v3.oas.annotations.OpenAPIDefinition
-import io.swagger.v3.oas.annotations.info.Info
-import io.swagger.v3.oas.annotations.servers.Server
 
-@OpenAPIDefinition(
-  info =
-    Info(
-      title = "WorkloadApi",
-      description = "API managing the workload",
-      version = "1.0.0",
-    ),
-  servers = [Server(url = "http://localhost:8007/api")],
-)
 class Application {
   companion object {
     @JvmStatic fun main(args: Array<String>) {
