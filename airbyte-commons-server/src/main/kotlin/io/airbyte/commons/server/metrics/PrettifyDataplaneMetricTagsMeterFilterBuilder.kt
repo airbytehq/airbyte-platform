2f2fdc6c6a9967eb9dfaadc6fec0da8104e6c6b0
Jimmy Ma <gosusnp@users.noreply.github.com>
2025-09-29 14:27:32 -0700

    chore: add job completed metrics (#17854)

--- airbyte-commons-server/src/main/kotlin/io/airbyte/commons/server/metrics/PrettifyDataplaneMetricTagsMeterFilterBuilder.kt
+++ airbyte-commons-server/src/main/kotlin/io/airbyte/commons/server/metrics/PrettifyDataplaneMetricTagsMeterFilterBuilder.kt
@@ -2,7 +2,7 @@
  * Copyright (c) 2020-2025 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.workload.metrics
+package io.airbyte.commons.server.metrics
 
 import io.airbyte.config.WorkloadConstants.Companion.PUBLIC_ORG_ID
 import io.airbyte.metrics.lib.MetricTags
