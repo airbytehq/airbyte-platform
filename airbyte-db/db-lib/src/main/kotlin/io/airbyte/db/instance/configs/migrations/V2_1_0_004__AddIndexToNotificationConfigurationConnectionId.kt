7f8790997dabed883fe2c23d4cdb5d9bf1c51eac
Jimmy Ma <gosusnp@users.noreply.github.com>
2025-10-14 13:17:10 -0700

    chore(db): add missing index on notification_configuration (#18019)

---
+++ airbyte-db/db-lib/src/main/kotlin/io/airbyte/db/instance/configs/migrations/V2_1_0_004__AddIndexToNotificationConfigurationConnectionId.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020-2025 Airbyte, Inc., all rights reserved.
+ */
+
+package io.airbyte.db.instance.configs.migrations
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+import org.jooq.impl.DSL
+
+private val log = KotlinLogging.logger {}
+
+/**
+ * Add index on notification_configuration.connection_id.
+ */
+@Suppress("ktlint:standard:class-naming")
+class V2_1_0_004__AddIndexToNotificationConfigurationConnectionId : BaseJavaMigration() {
+  override fun migrate(context: Context) {
+    log.info { "Running migration: ${javaClass.simpleName}" }
+
+    val ctx = DSL.using(context.connection)
+    ctx
+      .createIndexIfNotExists("notification_configuration_connection_id_idx")
+      .on("notification_configuration", "connection_id")
+      .execute()
+  }
+}
