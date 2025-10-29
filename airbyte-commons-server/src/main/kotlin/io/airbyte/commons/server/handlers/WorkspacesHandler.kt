dd3fb757d38a874bd8220377703c9551ce8d4afc
Jonathan Pearlin <jonathan@airbyte.io>
2025-10-29 15:09:15 -0400

    fix: invert condition (#18223)

--- airbyte-commons-server/src/main/kotlin/io/airbyte/commons/server/handlers/WorkspacesHandler.kt
+++ airbyte-commons-server/src/main/kotlin/io/airbyte/commons/server/handlers/WorkspacesHandler.kt
@@ -193,7 +193,7 @@ class WorkspacesHandler
           .withWebhookOperationConfigs(toPersistenceWrite(workspaceCreateWithId.webhookConfigs, uuidSupplier))
           .withOrganizationId(workspaceCreateWithId.organizationId)
 
-      if (email.isNullOrBlank()) {
+      if (!email.isNullOrBlank()) {
         workspace.withEmail(email)
       }
 
