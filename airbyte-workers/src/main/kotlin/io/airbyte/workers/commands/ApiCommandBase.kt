a32e6946f402d7c570b3f2aa3fda1e8f0ae5ff98
Jimmy Ma <gosusnp@users.noreply.github.com>
2025-12-16 10:50:26 -0800

    chore(airbyte-worker): add missing command error handling (#18432)

--- airbyte-workers/src/main/kotlin/io/airbyte/workers/commands/ApiCommandBase.kt
+++ airbyte-workers/src/main/kotlin/io/airbyte/workers/commands/ApiCommandBase.kt
@@ -5,9 +5,14 @@
 package io.airbyte.workers.commands
 
 import io.airbyte.api.client.AirbyteApiClient
+import io.airbyte.api.client.ApiException
 import io.airbyte.api.client.model.generated.CancelCommandRequest
 import io.airbyte.api.client.model.generated.CommandStatusRequest
 import io.airbyte.api.client.model.generated.CommandStatusResponse
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.micronaut.http.HttpStatus
+
+private val logger = KotlinLogging.logger {}
 
 abstract class ApiCommandBase<Input>(
   protected val airbyteApiClient: AirbyteApiClient,
@@ -20,8 +25,19 @@ abstract class ApiCommandBase<Input>(
   }
 
   override fun cancel(id: String) {
-    airbyteApiClient.commandApi.cancelCommand(
-      CancelCommandRequest(id),
-    )
+    try {
+      airbyteApiClient.commandApi.cancelCommand(
+        CancelCommandRequest(id),
+      )
+    } catch (e: ApiException) {
+      when (e.statusCode) {
+        HttpStatus.GONE.code -> logger.info { "Command $id already terminal. Cancellation is a no-op." }
+        HttpStatus.NOT_FOUND.code -> logger.info { "Command $id not found. Cancellation is a no-op." }
+        else -> {
+          logger.error(e) { "Command $id failed to be cancelled due to API error. Status code: ${e.statusCode}" }
+          throw e
+        }
+      }
+    }
   }
 }
