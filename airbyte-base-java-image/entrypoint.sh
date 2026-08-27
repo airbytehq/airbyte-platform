76a8af7b55e70253ea1184c47b68938180327d1a
Cam Kennedy <cameron.kennedy@airbyte.io>
2026-08-27 14:23:01 -0700

    chore: bump version + update deploy script (#19383)
    Co-authored-by: Christo Grabowski <christo.grab@gmail.com>
    Co-authored-by: Christo Grabowski <108154848+ChristoGrab@users.noreply.github.com>

--- airbyte-base-java-image/entrypoint.sh
+++ airbyte-base-java-image/entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-update-ca-trust
+D=/etc/pki/ca-trust/extracted/pem/directory-hash
+update-ca-trust || true
+chmod u+w "$D"
+ln -sf ../tls-ca-bundle.pem "$D/ca-certificates.crt"
+ln -sf ../tls-ca-bundle.pem "$D/ca-bundle.crt"
+chmod u-w "$D"
 
 exec "$@"
